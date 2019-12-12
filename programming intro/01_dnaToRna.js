286a1f90fde9f6be46672edaa28b71d4dabc3d69
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-12 12:23:37 +0300

    added: 01_dnaToRna.js

---
+++ programming intro/01_dnaToRna.js
@@ -0,0 +1,39 @@
+const dnaToRna = (dna) => {
+  let rna = '';
+  if (dna.length === 0) {
+    return rna;
+  }
+  for (let char of dna) {
+    switch (char.toUpperCase()) {
+      case 'A':
+        rna += 'U';
+        break;
+      case 'C':
+        rna += 'G';
+        break;
+      case 'G':
+        rna += 'C';
+        break;
+      case 'T':
+        rna += 'A';
+        break;
+      default:
+        return null;
+    }
+  }
+  return rna;
+}
+
+// dnaToRna testing
+const args = [
+  'ACGTGGTCTTAA',
+  'CCGTA',
+  '',
+  'ACNTG',
+  'acgtggtttttttttcttAA',
+  'ccgTA',
+  'acNTG'
+];
+args.forEach((elem) => {
+  console.log(`dnaToRna(${elem}) = ${dnaToRna(elem)}`);
+});
