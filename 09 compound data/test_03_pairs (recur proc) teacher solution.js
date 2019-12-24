9225af6915b9f07b4cddb8a8f14449b7c00bd0ca
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-24 22:29:55 +0300

    added: 09 compound data/test_03_pairs (recur proc) teacher solution.js

---
+++ 09 compound data/test_03_pairs (recur proc) teacher solution.js
@@ -0,0 +1,46 @@
+/* Пару можно создать на основе строки. Для хранения двух значений применим разделитель.
+Им может выступить любой символ, однако во избежание совпадений с исходными данными лучше взять
+редко используемое значение.
+Для этого подойдёт так называемая управляющая или escape-последовательность,
+которая начинается с обратной косой черты. Мы будем использовать специальный символ \t,
+обозначающий горизональную табуляцию.
+Функции car и cdr должны получить содержимое строки до и после разделителя соответственно.
+Управляющая последовательность воспринимается интерпретатором как одиночный символ, т.е. имеет длину, равную 1.
+Обязательным условием является отсутствие данного символа в строках, которые объединяются в пару.
+Реализуйте и экспортируйте следующие функции в соответствии с алгоритмом выше:
+cons
+car
+cdr
+Примеры
+const pair = cons('computer', 'science'); // 'computer\tscience'
+car(pair); // 'computer'
+cdr(pair); // 'science'
+Подсказки
+Для подсчёта длины строки используйте функцию length() из модуля strings.
+ */
+
+const length = (str) => str.length;
+const separator = '\t';
+const cons = (a, b) => `${a}${separator}${b}`;
+const getSeparatorIndex = (str) => {
+  const iter = (i) => str[i] === separator ? i : iter(i + 1);
+  return iter(0);
+};
+const getValue = (pair, begin, end) => {
+  const iter = (acc, i) => {
+    if (i >= end) {
+      return acc;
+    }
+    const newAcc = `${acc}${pair[i]}`;
+
+    return iter(newAcc, i + 1);
+  };
+  return iter('', begin);
+};
+const car = (pair) => getValue(pair, 0, getSeparatorIndex(pair));
+const cdr = (pair) => getValue(pair, getSeparatorIndex(pair) + 1, length(pair));
+
+// testing
+const pair = cons('comp', 'ram');
+console.log('car(pair) = ' + car(pair));
+console.log('cdr(pair) = ' + cdr(pair));
