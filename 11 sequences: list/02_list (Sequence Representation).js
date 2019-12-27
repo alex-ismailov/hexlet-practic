ec2954efe663b6ad49f35679a2c0dcc9dbb16b89
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-28 01:40:07 +0300

    added: function reverse()

--- 11 sequences: list/02_list (Sequence Representation).js
+++ 11 sequences: list/02_list (Sequence Representation).js
@@ -12,9 +12,27 @@ const has = (list, elem) => {
   return has(tail(list), elem);
 };
 
+const reverse = (list) => {
+  const iter = (list, acc) => {
+    if(tail(list) === null) {
+      return l(`${head(list)}${acc}`);
+    }
+    return iter(tail(list), `, ${head(list)}${acc}`);
+  }
+  return iter(list, '');
+};
+
+const list1 = l(13, 15, 17);
+const list2 = l(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(listToString(reverse(list1)));
+console.log(listToString(reverse(list2)));
+
+// const str = '13, 15, 17';
+// console.log(listToString(l(str)));
+
 // console.log(listToString(head(tail(numbers))));
 
-console.log(has(numbers, 1));
+// console.log(has(numbers, 1));
 
 // console.log(head(numbers));
 // console.log(tail(tail(tail(tail(tail(numbers))))));
