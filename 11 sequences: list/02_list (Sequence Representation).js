61189421e1b28a3e799eb80d85607ab8f7c7b06b
alex-ismailov <alexander.is.dev@gmail.com>
2019-12-31 09:15:55 +0300

    added: function -> concat

--- 11 sequences: list/02_list (Sequence Representation).js
+++ 11 sequences: list/02_list (Sequence Representation).js
@@ -3,32 +3,64 @@ import { l, cons, head, tail, isEmpty, toString as listToString } from '@hexlet/
 const numbers = l(1, 2, 3, 4, 5);
 
 const has = (list, elem) => {
+  if (isEmpty(list)) {
+    return false;
+  }
   if (head(list) === elem) {
     return true;
-  }
-  if (isEmpty(tail(list))) {
-    return false;
   }
   return has(tail(list), elem);
 };
 
 const reverse = (list) => {
-  const iter = (list, acc) => {
-    if (isEmpty(tail(list))) {
-      return cons(head(list), acc);
+  const iter = (restOfList, acc) => {
+    if (isEmpty(restOfList)) {
+      return acc;
     }
-    return iter(tail(list), cons(head(list), acc));
+    return iter(tail(restOfList), cons(head(restOfList), acc));
+  }
+  if (isEmpty(list)) {
+    return l();
   }
   return iter(tail(list), cons(head(list), l()));
 };
 
+const copy = (oldList) => cons(head(oldList), tail(oldList));
+
+const length = (list) => {
+  if (isEmpty(list)) {
+    return 0;
+  }
+  return 1 + length(tail(list));
+};
+
+const getElement = (list, n) => {
+  if (n >= length(list)) {
+    return 'item number exceeds list length, try again';
+  }
+  if (n === 0) {
+    return head(list);
+  }
+  return getElement(tail(list), n - 1);
+};
+
+const concat = (list1, list2) => {
+  if (isEmpty(list1)) {
+    return list2;
+  }
+  return cons(head(list1), concat(tail(list1), list2));
+};
 
 const list1 = l(1, 2, 3, 4, 5);
-console.log(listToString(list1));
-const revList = reverse(list1);
-console.log(listToString(revList));
+const list2 = l(1, 2, 3);
+const list3 = l();
+const listStr = l('one', 'two', 'three', 'four', 'five');
+console.log('length(list1) = ' + length(list1));
+console.log('length(list2) = ' + length(list2));
+console.log('length(list3) = ' + length(list3));
 
-console.log(listToString(tail(tail(tail(tail(revList))))));
-console.log(listToString(tail(tail(tail(revList)))));
-console.log(listToString(tail(tail(revList))));
-console.log(listToString(tail(revList)));
+console.log(getElement(list1, 4));
+console.log(getElement(listStr, 0));
+console.log(getElement(listStr, 4));
+console.log(getElement(listStr, 1));
+console.log(getElement(listStr, 5));
