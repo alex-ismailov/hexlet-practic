47bf2ac7f02162bb74192aee6a281e233093b332
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-03 02:53:49 +0300

    added: toString function, modified: arg names

--- 11 sequences: list/03_html-tags.js
+++ 11 sequences: list/03_html-tags.js
@@ -5,9 +5,9 @@ export const make = () => l();
 
 // node — создает новый тег. Содержит два элемента, имя тега и его содержимое. 
 // Дополнительно реализуйте селекторы тега: getName и getValue.
-const node = (name, value) => cons(name, value);
-const getName = (node) => car(node);
-const getValue = (node) => cdr(node);
+const node = (name, content) => cons(name, content);
+const getName = (tag) => car(tag);
+const getValue = (tag) => cdr(tag);
 // *** node, getName, getValue tests ***
 const tag = node('div', 'what is love?');
 console.log(getName(tag));
@@ -17,7 +17,7 @@ console.log('-----\n');
 
 // append — добавляет элемент (тег), созданный с помощью node, в html-список. 
 // Возвращает новый html-список. Новый элемент должен добавляться в начало ("голову") списка.
-const append = (dom, node) => consList(node, dom);
+const append = (dom, tag) => consList(tag, dom);
 // *** append test ***
 const dom1 = make();
 const div = node('div', 'Baby don`t hurt me');
@@ -26,3 +26,42 @@ console.log(listToString(dom2));
 console.log(pairToString(dom2));
 console.log('-----\n');
 // ***
+
+// toString — возвращает текстовое представление html на основании html-списка.
+// const toString = (currentDom) => {
+//   const iter = (dom, acc) => {
+//     if (isEmpty(dom)) {
+//       return acc;
+//     }
+//     return iter(
+//       tail(dom),
+//       `<${car(head(dom))}>${cdr(head(dom))}</${car(head(dom))}>${acc}`,
+//     );
+//   };
+//   return iter(currentDom, '');
+// };
+
+const toString = (currentDom) => {
+  const iter = (dom, acc) => (
+    isEmpty(dom) ? acc : iter(
+      tail(dom),
+      `<${car(head(dom))}>${cdr(head(dom))}</${car(head(dom))}>${acc}`,
+    )
+  );
+  return iter(currentDom, '');
+};
+
+// *** toString test ***
+const p = node('p', 'Don`t hurt me no more');
+const dom3 = append(dom2, p);
+console.log(toString(dom3));
+
+// console.log(pairToString(dom3));
+// console.log(pairToString(head(dom3)));
+// console.log(pairToString(tail(dom3)));
+
+// console.log(car(head(dom3)));
+// console.log(cdr(head(dom3)));
+
+// console.log(car(head(tail(dom3))));
+// console.log(cdr(head(tail(dom3))));
