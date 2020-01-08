252549fc2691359dabe63d6a2e9322228270b925
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-08 04:37:48 +0300

    version 2 - tests fail

--- 11 sequences: list/08 select (tree recursion).js
+++ 11 sequences: list/08 select (tree recursion).js
@@ -1,5 +1,5 @@
 import { /* eslint-disable */
-  l, cons as consList, isList, isEmpty, head, tail, concat, toString as listToString,
+  l, cons as consList, isList, isEmpty, head, tail, concat, toString as listToString, length
 } from '@hexlet/pairs-data';
 
 import {
@@ -8,43 +8,43 @@ import {
 } from '@hexlet/html-tags'; /* eslint-enable */
 
 // BEGIN (write your solution here)
-// const fn = (elem, acc) => {
-//   if (!hasChildren(elem)) {
-//     return consList(getName(elem), acc);
-//   }
-//   return concat(select(children(elem)), consList(getName(elem), acc));
-// }
-
-const isLeaf = (elem, acc) => {
-  if (!hasChildren(elem)) {
-    return consList(elem, acc);
-  }
-  return concat(select(children(elem)), acc);
-}
-
-const select = (list) => {
+export const select = (tag, list) => {
   if (isEmpty(list)) {
-    returnl();
+    return l();
   }
-  return reduce(isLeaf, l(), list)
+  const isLeaf = (elem, acc) => {
+    if (!hasChildren(elem)) {
+      return consList(elem, acc);
+    }
+    return concat(select(tag, children(elem)), consList(elem, acc));
+  }
+  const linearList = reduce(isLeaf, l(), list);
+  const filtered = filter((elem) => is(tag, elem), linearList);
+
+  return filtered;
 }
 // END
 
-const dom1 = make(); // Список нод, то есть это лес, а не дерево
-const dom2 = append(dom1, node('h1', 'scheme'));
-const dom3 = append(dom2, node('p', 'is a lisp'));
-
-const children1 = l(node('li', 'item 1'), node('li', 'item 2'));
-const dom4 = append(dom3, node('ul', children1));
-const children2 = l(node('li', 'item 1'), node('li', 'item 2'));
-const dom5 = append(dom4, node('ol', children2));
-const dom6 = append(dom5, node('p', 'is a functional language'));
-const children3 = l(node('li', 'item'));
-const dom7 = append(dom6, node('ul', children3));
-const dom8 = append(dom7, node('div', l(node('p', 'text'))));
-const dom9 = append(dom8, node('div', l(node('div', l(node('p', 'text'))))));
-
+    const dom1 = make();
+    const children1 = l(node('a', l(node('span', 'scheme'))));
+    const dom2 = append(dom1, node('h1', children1));
+    const dom3 = append(dom2, node('p', 'is a lisp'));
+    const children2 = l(node('li', 'item 1'), node('li', 'item 2'));
+    const dom4 = append(dom3, node('ul', children2));
+    const children3 = l(node('li', 'item 1'), node('li', 'item 2'));
+    const dom5 = append(dom4, node('ol', children3));
+    const dom6 = append(dom5, node('p', 'is a functional language'));
+    const children4 = l(node('li', 'item'));
+    const dom7 = append(dom6, node('ul', children4));
+    const dom8 = append(dom7, node('div', l(node('p', 'another text'))));
+    const dom9 = append(dom8, node('div', l(node('div', l(node('p', l(node('span', 'text'))))))));
 const dom10 = append(dom9, node('h1', 'prolog'));
 const dom = append(dom10, node('p', 'is about logic'));
 
-console.log(listToString(select(dom)));
+console.log(length(select('span', dom))) // toBe(2);
+    console.log(length(select('section', dom))) // toBe(0);
+    console.log(length(select('li', dom))) // toBe(5);
+    console.log(length(select('p', dom))) // toBe(5);
+    console.log(length(select('h1', dom))) // toBe(2);
+    console.log(length(select('div', dom))) // toBe(3);
+    console.log(htmlToString(select('div', dom))) // toBe(3);
