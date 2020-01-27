783d5b622ca42c40a2ace7704b87acf993c3bc1b
alex-ismailov <alexander.is.dev@gmail.com>
2020-01-28 00:22:40 +0300

    new tests was added

--- 10 functions/05 union (spread - unpacking).js
+++ 10 functions/05 union (spread - unpacking).js
@@ -55,9 +55,9 @@ const union = (...arrays) => uniq([].concat(...arrays));
 // };
 
 /* testing */
-// console.log(union([3])); // [3]
+console.log(union([3])); // [3]
 console.log(union([3, 2], [2, 2, 1])); // [3, 2, 1]
-// console.log(union(['a', 3, false], [true, false, 3], [false, 5, 8])); // ['a', 3, false, true, 5, 8]
+console.log(union(['a', 3, false], [true, false, 3], [false, 5, 8])); // ['a', 3, false, true, 5, 8]
 
 // const array1 = [];
 // const array2 = ['d', 'e', 'f'];
