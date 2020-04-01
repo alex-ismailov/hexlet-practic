ecb42299753dec1d51d2fa18ddebd51b1836b345
smilenb <alexander.is.dev@gmail.com>
2020-04-01 16:52:04 +0300

    chore(12 tree/) test 04 itinerary.js

--- 12 trees/test 04 itinerary.js
+++ 12 trees/test 04 itinerary.js
@@ -44,50 +44,22 @@ const tree = ['Moscow', [
 ]];
 
 /* teacher */
-const getAdjacencyList2 = (tree, parent = null) => {
+const getAdjacencyList = (tree, parent = null) => {
   const [leaf, children] = tree;
 
   if (!children) {
     return { [leaf]: [parent] };
   }
   
-  console.log(leaf);
-  console.log(children);
   const flatChildren = _.flatten(children);
-  console.log(flatChildren);
-  // console.log([...flatChildren]);
-  // const neighbors = [...flatChildren] // не понимаю зачем вот это - [...flatChildren]? если это и так уже массив.
-  //   .filter((n) => n && !_.isArray(n));
-
   const neighbors = flatChildren // не понимаю зачем вот это - [...flatChildren]?
     .filter((n) => n && !_.isArray(n));
 
-  console.log(neighbors);
-  console.log('-----------------------------------------');
   return {
     [leaf]: [parent, [...neighbors]],
-    ...children.reduce((acc, c) => ({ ...acc, ...getAdjacencyList2(c, leaf) }), {}),
+    ...children.reduce((acc, c) => ({ ...acc, ...getAdjacencyList(c, leaf) }), {}),
   };
 };
-
-// const getAdjacencyList = (tree) => {
-//   const adjacencyList = {};
-//   const makeAdjacencyList = (currTree, dict, parent = null) => {
-//     const [nodeName, branches] = currTree;
-//     const children = [];
-//     _.set(dict, nodeName, [parent, children]);
-//     if (branches) {
-//       branches.forEach((branch) => {
-//         const name = makeAdjacencyList(branch, dict, nodeName);
-//         children.push(name);
-//       });
-//     }
-//     return nodeName;
-//   };
-
-//   makeAdjacencyList(tree, adjacencyList);
-//   return adjacencyList;
-// };
 
 const getCommonParent = (first, second, dict) => {
   const [parentName] = dict[first];
@@ -116,7 +88,7 @@ const getRoute = (from, to, dict) => {
 };
 
 const itinerary = (tree, from, to) => {
-  const adjacencyList = getAdjacencyList2(tree);
+  const adjacencyList = getAdjacencyList(tree);
   const commonParent = getCommonParent(from, to, adjacencyList);
   const upRoute = getRoute(from, commonParent, adjacencyList);
   const downRoute = getRoute(to, commonParent, adjacencyList).reverse();
@@ -124,16 +96,12 @@ const itinerary = (tree, from, to) => {
   return _.uniq([...upRoute, ...downRoute]);
 };
 
-// console.log(getAdjacencyList(tree));
-
-console.log('====================');
 /* testing */
-// console.log(itinerary(tree, 'Dubna', 'Kostroma'));
+console.log(itinerary(tree, 'Dubna', 'Kostroma'));
 // ['Dubna', 'Tver', 'Moscow', 'Ivanovo', 'Kostroma']
 
-console.log(`answer: ${itinerary(tree, 'Borisovka', 'Kurchatov')}`);
+console.log(itinerary(tree, 'Borisovka', 'Kurchatov'));
 // ['Borisovka', 'Belgorod', 'Kursk', 'Kurchatov']
 
-// console.log(getAdjacencyList(tree));
-// console.log('***********');
-// console.log(getAdjacencyList2(tree));
+console.log(itinerary(tree, 'Klin', 'Rzhev'));
+// [ 'Klin', 'Tver', 'Rzhev' ]
