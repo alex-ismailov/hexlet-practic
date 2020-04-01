c0cb09f55d6c1ae84db5b3f228d1e06233fce29d
smilenb <alexander.is.dev@gmail.com>
2020-04-01 12:12:11 +0300

    refactor(12 trees/test 04 itinerary.js) getAdjacencyList2 - teacher solution

--- 12 trees/test 04 itinerary.js
+++ 12 trees/test 04 itinerary.js
@@ -21,24 +21,73 @@ import _ from 'lodash';
 //   Rzhev: ['Tver', []],
 // };
 
-const getAdjacencyList = (tree) => {
-  const adjacencyList = {};
-  const makeAdjacencyList = (currTree, dict, parent = null) => {
-    const [nodeName, branches] = currTree;
-    const children = [];
-    _.set(dict, nodeName, [parent, children]);
-    if (branches) {
-      branches.forEach((branch) => {
-        const name = makeAdjacencyList(branch, dict, nodeName);
-        children.push(name);
-      });
-    }
-    return nodeName;
-  };
+const tree = ['Moscow', [
+  ['Smolensk'],
+  ['Yaroslavl'],
+  ['Voronezh', [
+    ['Liski'],
+    ['Boguchar'],
+    ['Kursk', [
+      ['Belgorod', [
+        ['Borisovka'],
+      ]],
+      ['Kurchatov'],
+    ]],
+  ]],
+  ['Ivanovo', [
+    ['Kostroma'], ['Kineshma'],
+  ]],
+  ['Vladimir'],
+  ['Tver', [
+    ['Klin'], ['Dubna'], ['Rzhev'],
+  ]],
+]];
 
-  makeAdjacencyList(tree, adjacencyList);
-  return adjacencyList;
+/* teacher */
+const getAdjacencyList2 = (tree, parent = null) => {
+  const [leaf, children] = tree;
+
+  if (!children) {
+    return { [leaf]: [parent] };
+  }
+  
+  console.log(leaf);
+  console.log(children);
+  const flatChildren = _.flatten(children);
+  console.log(flatChildren);
+  // console.log([...flatChildren]);
+  // const neighbors = [...flatChildren] // не понимаю зачем вот это - [...flatChildren]? если это и так уже массив.
+  //   .filter((n) => n && !_.isArray(n));
+
+  const neighbors = flatChildren // не понимаю зачем вот это - [...flatChildren]?
+    .filter((n) => n && !_.isArray(n));
+
+  console.log(neighbors);
+  console.log('-----------------------------------------');
+  return {
+    [leaf]: [parent, [...neighbors]],
+    ...children.reduce((acc, c) => ({ ...acc, ...getAdjacencyList2(c, leaf) }), {}),
+  };
 };
+
+// const getAdjacencyList = (tree) => {
+//   const adjacencyList = {};
+//   const makeAdjacencyList = (currTree, dict, parent = null) => {
+//     const [nodeName, branches] = currTree;
+//     const children = [];
+//     _.set(dict, nodeName, [parent, children]);
+//     if (branches) {
+//       branches.forEach((branch) => {
+//         const name = makeAdjacencyList(branch, dict, nodeName);
+//         children.push(name);
+//       });
+//     }
+//     return nodeName;
+//   };
+
+//   makeAdjacencyList(tree, adjacencyList);
+//   return adjacencyList;
+// };
 
 const getCommonParent = (first, second, dict) => {
   const [parentName] = dict[first];
@@ -67,7 +116,7 @@ const getRoute = (from, to, dict) => {
 };
 
 const itinerary = (tree, from, to) => {
-  const adjacencyList = getAdjacencyList(tree);
+  const adjacencyList = getAdjacencyList2(tree);
   const commonParent = getCommonParent(from, to, adjacencyList);
   const upRoute = getRoute(from, commonParent, adjacencyList);
   const downRoute = getRoute(to, commonParent, adjacencyList).reverse();
@@ -75,31 +124,16 @@ const itinerary = (tree, from, to) => {
   return _.uniq([...upRoute, ...downRoute]);
 };
 
-/* testing */
-const tree = ['Moscow', [
-  ['Smolensk'],
-  ['Yaroslavl'],
-  ['Voronezh', [
-    ['Liski'],
-    ['Boguchar'],
-    ['Kursk', [
-      ['Belgorod', [
-        ['Borisovka'],
-      ]],
-      ['Kurchatov'],
-    ]],
-  ]],
-  ['Ivanovo', [
-    ['Kostroma'], ['Kineshma'],
-  ]],
-  ['Vladimir'],
-  ['Tver', [
-    ['Klin'], ['Dubna'], ['Rzhev'],
-  ]],
-]];
+// console.log(getAdjacencyList(tree));
 
-console.log(itinerary(tree, 'Dubna', 'Kostroma'));
+console.log('====================');
+/* testing */
+// console.log(itinerary(tree, 'Dubna', 'Kostroma'));
 // ['Dubna', 'Tver', 'Moscow', 'Ivanovo', 'Kostroma']
 
-console.log(itinerary(tree, 'Borisovka', 'Kurchatov'));
+console.log(`answer: ${itinerary(tree, 'Borisovka', 'Kurchatov')}`);
 // ['Borisovka', 'Belgorod', 'Kursk', 'Kurchatov']
+
+// console.log(getAdjacencyList(tree));
+// console.log('***********');
+// console.log(getAdjacencyList2(tree));
