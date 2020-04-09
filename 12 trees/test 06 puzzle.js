df2368652a06c824776bfc1c045350b85035e3e4
smilenb <alexander.is.dev@gmail.com>
2020-04-09 21:01:08 +0300

    refactor(12 trees/test 06 puzzle.js) use _.union instead of customizer

--- 12 trees/test 06 puzzle.js
+++ 12 trees/test 06 puzzle.js
@@ -10,47 +10,47 @@ import _ from 'lodash';
 
 /* data */
 const branch1 = ['A', [ //   A
-  ['B', [ //   |
-    ['C'], //   B
-    ['D'], //  / \
-  ]], // C   D
+  ['B', [               //   |
+    ['C'],              //   B
+    ['D'],              //  / \
+  ]],                   // C   D
 ]];
 
 const branch2 = ['B', [ //   B
-  ['D', [ //   |
-    ['E'], //   D
-    ['F'], //  / \
-  ]], // E   F
+  ['D', [               //   |
+    ['E'],              //   D
+    ['F'],              //  / \
+  ]],                   // E   F
 ]];
 
 const branch3 = ['I', [ //   I
-  ['A', [ //   |
-    ['B', [ //   A
-      ['C'], //   |
-      ['H'], //   B
-    ]], //  / \
-  ]], // C   H
+  ['A', [               //   |
+    ['B', [             //   A
+      ['C'],            //   |
+      ['H'],            //   B
+    ]],                 //  / \
+  ]],                   // C   H
 ]];
 
-const resTree = ['A', [ //     A
-  ['B', [ //    / \
-    ['C'], //   B   I
-    ['D', [ //  /|\
-      ['E'], // C D H
-      ['F'], //  / \
-    ]], // E   F
-    ['H'],
-  ]],
-  ['I'],
-]];
 
+const resTree = ['A', [      //     A
+                  ['B', [    //    / \
+                    ['C'],   //   B   I
+                    ['D', [  //  /|\
+                      ['E'], // C D H
+                      ['F'], //  / \
+                    ]],      // E   F
+                    ['H'],
+                  ]],
+                  ['I'],
+                ]];
+
+/* solution */
 const makeJoints = (tree, parent) => {
   const [node, children] = tree;
-
   if (!children) {
     return { [node]: [parent] };
   }
-
   const flattenChildren = _.flatten(children);
   const neighbors = [...flattenChildren, parent].filter((c) => c && !_.isArray(c));
 
@@ -71,19 +71,12 @@ const buildTree = (adjacencyList, node) => {
   return iter(node, []);
 };
 
-const customizer = (objValue, srcValue) => {
-  return !objValue || objValue.includes(srcValue)
-    ? objValue
-    : _.union(objValue, srcValue);
-};
-
 const combine = (...trees) => {
   const [first] = trees;
   const [root] = first;
   const joints = trees
     .map((tree) => makeJoints(tree))
-    .reduce((acc, currentJoints) => _.mergeWith(acc, currentJoints, customizer), {});
-
+    .reduce((acc, currentJoints) => _.mergeWith(acc, currentJoints, _.union), {});
   const newTree = buildTree(joints, root);
 
   return newTree;
@@ -95,54 +88,3 @@ console.log(JSON.stringify(combine(branch1, branch2, branch3)));
 
 console.log(JSON.stringify(combine(branch2, branch1, branch3)));
 console.log(JSON.stringify(combine(branch3, branch2, branch1)));
-
-/* ************ */
-// (objValue, srcValue, key, object, source, stack).
-// return _.mergeWith({}, acc, jointsFromBranch, _.union);
-
-const combine2 = (...trees) => {
-  const [first] = trees;
-  const [root] = first;
-  const joints = trees
-    .map((tree) => makeJoints(tree))
-    .reduce((acc, currentJoints) => {
-      return _.mergeWith(acc, currentJoints, _.union)
-    }, {});
-
-  console.log(joints);
-  const newTree = buildTree(joints, root);
-
-  return newTree;
-};
-
-// const combine2 = (...trees) => {
-//   const [first] = trees;
-//   const [root] = first;
-//   const joints = trees
-//     .map((tree) => makeJoints(tree))
-//     .reduce((acc, currentJoints) => {
-//       console.log('acc');
-//       console.log(acc);
-//       console.log('currentJoints');
-//       console.log(currentJoints);
-//       return _.mergeWith({}, acc, currentJoints, _.union)
-//     }, {});
-
-//   console.log(joints);
-//   const newTree = buildTree(joints, root);
-
-//   return newTree;
-// };
-
-// console.log('%%%%');
-// console.log(JSON.stringify(combine2(branch1, branch2, branch3)));
-
-// const fn = (...trees) => {
-//   const [first] = trees;
-//   const [root] = first;
-
-//   console.log(JSON.stringify(first));
-//   console.log(root);
-// };
-
-// fn(branch1, branch2, branch3);
