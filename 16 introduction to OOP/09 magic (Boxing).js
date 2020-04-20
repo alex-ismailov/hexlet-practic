a14cb3241e8084bd1ded64f01697dc0bf7ea74b8
smilenb <alexander.is.dev@gmail.com>
2020-04-20 22:04:46 +0300

    refactor(16 introduction to OOP/) 09 magic (Boxing).js

--- 16 introduction to OOP/09 magic (Boxing).js
+++ 16 introduction to OOP/09 magic (Boxing).js
@@ -12,10 +12,10 @@ magic(4, 8, 1, -1, -8)(3)(-3)(7, 2) == 13; // true
 Для решения задачи вам понадобится создать ещё одну функцию (внутреннюю). */
 
 const magic = (...numbers) => {
-  const sum = numbers.reduce((acc, x) => x + acc, 0);
-  const inner = (...rest) => magic(sum, ...rest);
-  inner.valueOf = () => sum;
-  return inner;
+  const sum = numbers.reduce((acc, num) => num + acc, 0);
+  const magicCaller = (...rest) => magic(sum, ...rest);
+  magicCaller.valueOf = () => sum;
+  return magicCaller;
 };
 
 /* testing */
