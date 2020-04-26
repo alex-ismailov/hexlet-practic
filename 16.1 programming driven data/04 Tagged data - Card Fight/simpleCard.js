c7d2bf106ca6e139fa7f83c8bee674b0dee36cfd
smilenb <alexander.is.dev@gmail.com>
2020-04-26 16:56:41 +0300

    style(16.1 programming driven data/04 Tagged data - Card Fight/simpleCard.js) add empty strings

--- 16.1 programming driven data/04 Tagged data - Card Fight/simpleCard.js
+++ 16.1 programming driven data/04 Tagged data - Card Fight/simpleCard.js
@@ -3,6 +3,8 @@ import { attach, contents } from '@hexlet/tagged-types';
 
 // BEGIN (write your solution here)
 export const make = (name, damage) => attach('SimpleCard', cons(name, damage));
+
 export const getName = (self) => car(contents(self));
+
 export const damage = (self) => cdr(contents(self));
 // END
