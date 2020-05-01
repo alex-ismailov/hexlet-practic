2173f3b6d561a7275fcf70e6249562c02a677a47
smilenb <alexander.is.dev@gmail.com>
2020-05-02 00:06:10 +0300

    refactor(js sicp/05 collections/) test 10 without/

--- js sicp/05 collections/test 10 without/test 10 without.js
+++ js sicp/05 collections/test 10 without/test 10 without.js
@@ -5,4 +5,12 @@
 without([2, 1, 2, 3], 1, 2, 5);
 // → [3] */
 
-export default (coll, ...rest) => coll.filter((item) => !rest.includes(item));
+// export default (coll, ...rest) => coll.filter((item) => !rest.includes(item));
+
+/* teacher solution */
+const without = (coll, ...rest) => {
+  const set = new Set(rest);
+  return coll.filter((num) => !set.has(num));
+};
+
+export default without;
