ec06635104e6d3952b13818adabfab7225e6a164
smilenb <alexander.is.dev@gmail.com>
2020-05-23 10:37:30 +0300

    add(03 arrays/) test 06 +NEW - rotate matrix.js

---
+++ 03 arrays/test 06 +NEW - rotate matrix.js
@@ -0,0 +1,74 @@
+/* matrix.js
+Реализуйте и экспортируйте функции rotateLeft и rotateRight, которые поворачивают матрицу влево
+(против часовой стрелки) и соответственно вправо (по часовой стрелке).
+
+Матрица реализована на массивах.
+Функции должны возвращать новую матрицу не изменяя исходную.
+Примеры:
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 0, 1, 2],
+];
+
+rotateLeft(matrix);
+// [
+//   [4, 8, 2],
+//   [3, 7, 1],
+//   [2, 6, 0],
+//   [1, 5, 9],
+// ]
+
+rotateRight(matrix);
+// [
+//   [9, 5, 1],
+//   [0, 6, 2],
+//   [1, 7, 3],
+//   [2, 8, 4],
+// ]
+*/
+
+// BEGIN (write your solution here)
+export const rotateLeft = (matrix) => matrix[0].map((col, i) => matrix.map((row) => row[i])).reverse();
+export const rotateRight = (matrix) => matrix[0].map((col, i) => matrix.map((row) => row[i]).reverse());
+// END
+
+
+/* testing */
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 0, 1, 2],
+];
+
+console.log(rotateLeft(matrix));
+// [
+//   [4, 8, 2],
+//   [3, 7, 1],
+//   [2, 6, 0],
+//   [1, 5, 9],
+// ]
+
+console.log(rotateRight(matrix));
+// [
+//   [9, 5, 1],
+//   [0, 6, 2],
+//   [1, 7, 3],
+//   [2, 8, 4],
+// ]
+
+const matrix2 = [
+  [1, 2, 3, 4, 5, 6],
+  [7, 8, 9, 0, 1, 2],
+  [3, 4, 5, 6, 7, 8],
+  [9, 0, 1, 2, 3, 4],
+];
+console.log(rotateLeft(matrix2));
+// const expected = [
+//   [6, 2, 8, 4],
+//   [5, 1, 7, 3],
+//   [4, 0, 6, 2],
+//   [3, 9, 5, 1],
+//   [2, 8, 4, 0],
+//   [1, 7, 3, 9],
+// ];
