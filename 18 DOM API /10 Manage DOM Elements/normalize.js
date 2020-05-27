5755c22affe1dad2fc8be78c919d0cac5c0219e5
smilenb <alexander.is.dev@gmail.com>
2020-05-27 22:38:27 +0300

    start(18 DOM API /) 10 Manage DOM Elements/

---
+++ 18 DOM API /10 Manage DOM Elements/normalize.js
@@ -0,0 +1,27 @@
+/* normalize.js
+Реализуйте и экспортируйте по умолчанию функцию normalize, которая нормализует имена классов для
+всех элементов на странице. В данном случае это означает, что происходит преобразование всех классов,
+написанных с использованием kebab нотации, в camelCase нотацию: text-center => textCenter
+
+Попробуйте решить эту задачу без использования регулярных выражений.
+
+// <body>
+//   <div class="text-center row-b">Bam</div>
+// </body>
+normalize(document);
+console.log(document.body.innerHTML);
+// <body>
+//   <div class="textCenter rowB">Bam</div>
+// </body>
+Подсказки
+Самый простой способ найти все элементы в документе это document.body.getElementsByTagName('*')
+Приведение к camelCase https://lodash.com/docs#camelCase
+Замена классов replace у объекта classList */
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { camelCase } from 'lodash';
+
+// BEGIN (write your solution here)
+
+// END
