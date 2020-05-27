e203e124dfa3f9358f96269f58ed8e2db034686f
smilenb <alexander.is.dev@gmail.com>
2020-05-27 23:55:25 +0300

    add(18 DOM API /10 Manage DOM Elements/normalize.js) teacher solution

--- 18 DOM API /10 Manage DOM Elements/normalize.js
+++ 18 DOM API /10 Manage DOM Elements/normalize.js
@@ -32,5 +32,13 @@ const normalize = (doc) => {
   });
 };
 
+/* teacher solution */
+// const normalize = (doc) => {
+//   for (const element of doc.body.getElementsByTagName('*')) { // eslint-disable-line
+//     const process = (item) => element.classList.replace(item, camelCase(item));
+//     element.classList.forEach(process);
+//   }
+// };
+
 export default normalize;
 // END
