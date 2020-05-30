5424d77718a626d0e075af48cfd29d16fd3d0212
smilenb <alexander.is.dev@gmail.com>
2020-05-31 01:19:41 +0300

    add(18 DOM API /14 Interception and Ascent/src/application.js) teacher solution

--- 18 DOM API /14 Interception and Ascent/src/application.js
+++ 18 DOM API /14 Interception and Ascent/src/application.js
@@ -114,3 +114,30 @@ export default () => {
   });
 };
 // END
+
+/* teacher solution */
+// const getDistance = (a, b) => Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+
+// export default () => {
+//   let currentPosition = { x: 3, y: 3 };
+//   const tableEl = generatePlayingField();
+
+//   tableEl.addEventListener('click', (e) => {
+//     const cell = e.target;
+//     const { cellIndex, parentElement: { rowIndex } } = cell;
+//     const newPosition = { y: rowIndex, x: cellIndex };
+//     const distance = getDistance(currentPosition, newPosition);
+//     if (distance !== 1) {
+//       return;
+//     }
+//     const point = tableEl.rows[currentPosition.y].cells[currentPosition.x];
+//     point.textContent = cell.textContent;
+//     point.classList.remove('table-active');
+//     cell.textContent = '';
+//     cell.classList.add('table-active');
+//     currentPosition = { x: cellIndex, y: rowIndex };
+//   });
+
+//   const root = document.querySelector('.gem-puzzle');
+//   root.append(tableEl);
+// };
