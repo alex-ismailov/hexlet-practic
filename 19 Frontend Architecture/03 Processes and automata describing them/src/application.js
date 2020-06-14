b848cde7ca9cecc836d01eefa4b010def94225cd
smilenb <alexander.is.dev@gmail.com>
2020-06-14 13:27:38 +0300

    refactor(19 Frontend Architecture/03 Processes and automata describing them/src/application.js) i`ve got solution !!!

--- 19 Frontend Architecture/03 Processes and automata describing them/src/application.js
+++ 19 Frontend Architecture/03 Processes and automata describing them/src/application.js
@@ -45,43 +45,14 @@ src/application.js
 /* eslint-disable no-param-reassign */
 
 // BEGIN (write your solution here)
-const render = (state, element, elementName, content) => {
-  console.log(element);
-  if (state.registrationProcess[elementName] === 'filling') {
-    const form = buildForm(elementName);
-    // form.addEventListener('submit', (e) => handleFormOnSubmit(e, elementName, state));
-    // form.addEventListener('input', (e) => handleFormOnInput(e, elementName, state));
-    element.innerHTML = '';
-    element.appendChild(form);
-    element.firstChild.addEventListener('submit', (e) => handleFormOnSubmit(e, elementName, state));
-    element.firstChild.addEventListener('input', (e) => handleFormOnInput(e, elementName, state));
-    // element.innerHTML = form.outerHTML;
-    console.log('in render');
-    console.log(element);
-    
- 
-    return;
-  }
-  if (state.registrationProcess[elementName] === 'finished') {
-    console.log(content);
-    element.innerHTML = content;
-    console.log(element);
-    return;
-  }
-  if (state.registrationProcess[elementName] = 'disabled') {
-    element.innerHTML = '';
-    element.innerHTML = content;
-    element.firstChild.addEventListener('click', (e) => handleDivOnClick(state, e));
-    return;
-  }
-};
-
+// BEGIN (write your solution here)
 const buildForm = (nameOfInput) => {
-  const form = document.createElement('form')//.focus();
-  const input = document.createElement('input')//.focus();
+  const form = document.createElement('form');// .focus();
+  const input = document.createElement('input');// .focus();
   input.type = 'text';
   input.name = nameOfInput;
-  // input.value = '';
+  input.setAttribute('value', '');
+
   const submit = document.createElement('input');
   submit.type = 'submit';
   submit.value = 'Save';
@@ -90,44 +61,75 @@ const buildForm = (nameOfInput) => {
   return form;
 };
 
-const handleFormOnSubmit = (e, elementName, state) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  let content = formData.get(elementName);
-  state.registrationProcess[elementName] = 'finished';
-  console.log(e.target.parentNode);
-  render(state, e.target.parentNode, elementName, content);
-};
+const render = (state, element, elementName, handle, content) => {
+  if (state.registrationProcess[elementName] === 'filling') {
+    const form = buildForm(elementName);
+    form.addEventListener('submit', handle.onSubmit(state, elementName));
+    form.addEventListener('input', handle.onInput(state, elementName));
 
-const handleFormOnInput = (e, elementName, state) => {
-  let content = e.target.value;
-  state.registrationProcess[elementName] = 'disabled';
-  if (content === '') {
-    const newContent = `<i>${elementName}</i>`;
-    const div = e.target.closest('div');
-    render(state, div, elementName, newContent);
+    element.innerHTML = '';
+    element.append(form);
+
+    return;
+  }
+  if (state.registrationProcess[elementName] === 'finished') {
+    element.innerHTML = content;
+    element.addEventListener('click', handle.onClick(state), { once: true });
+    return;
+  }
+  if (state.registrationProcess[elementName] === 'disabled') {
+    element.innerHTML = '';
+    element.innerHTML = `<i>${elementName}</i>`;
+    element.addEventListener('click', handle.onClick(state), { once: true });
   }
 };
 
-const handleDivOnClick = (state, e) => {
-    const elementName = e.target.parentNode.dataset.editableTarget;
-    state.registrationProcess[elementName] = 'filling';
-    render(state, e.target.parentNode, elementName);
+const handle = {
+  onClick: (state) => (e) => {
+    const div = e.currentTarget;
+    const divName = div.dataset.editableTarget;
+    state.registrationProcess[divName] = 'filling';
+
+    render(state, div, divName, handle);
+  },
+  onSubmit: (state, elementName) => (e) => {
+    e.preventDefault();
+    state.registrationProcess[elementName] = 'finished';
+    const divWrapper = document.querySelector(`div[data-editable-target="${elementName}"]`);
+    const formData = new FormData(e.target);
+    const content = formData.get(elementName);
+
+    if (content === '') {
+      state.registrationProcess[elementName] = 'disabled';
+      const defaultContent = elementName;
+      render(state, divWrapper, elementName, handle, defaultContent)
+    }
+
+    render(state, divWrapper, elementName, handle, content);
+  },
+  onInput: (state, elementName) => (e) => {
+    state.registrationProcess[elementName] = 'disabled';
+    const content = e.target.value;
+    const divWrapper = document.querySelector(`div[data-editable-target="${elementName}"]`);
+    if (content === '') {
+      const defaultContent = elementName;
+      render(state, divWrapper, elementName, handle, defaultContent);
+    }
+  },
 };
-  
+
 const app = () => {
   const state = {
-    registrationProcess: {       
+    registrationProcess: {
       name: 'disabled',
       email: 'disabled',
-    }
+    },
   };
 
-  const elements = document.querySelectorAll('div[data-editable-target] i');
-  elements.forEach((element) => element.addEventListener('click', (e) => handleDivOnClick(state, e)));
+  const elements = document.querySelectorAll('div[data-editable-target]');
+  elements.forEach((element) => element.addEventListener('click', handle.onClick(state), { once: true }));
 };
 
-// export default app;
 export default app;
 // END
 
