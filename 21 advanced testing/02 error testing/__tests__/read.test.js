319375b13366aceb6a944aefea9cd047e509a97c
smilenb <alexander.is.dev@gmail.com>
2020-06-16 18:38:50 +0300

    21 advanced testing/02 error testing/__tests__/) read.test.js, add teacher style

--- 21 advanced testing/02 error testing/__tests__/read.test.js
+++ 21 advanced testing/02 error testing/__tests__/read.test.js
@@ -3,14 +3,8 @@ import getFunction from '../functions.js';
 const read = getFunction();
 
 // BEGIN (write your solution here)
-test('file not found', () => {
-  expect(() => {
-    read('/undefined');
-  }).toThrow();
-});
-test('it is function', () => {
-  expect(() => {
-    read('/etc');
-  }).toThrow();
+test('read', () => {
+  expect(() => read('/undefined')).toThrow();
+  expect(() => read('/etc')).toThrow();
 });
 // END
