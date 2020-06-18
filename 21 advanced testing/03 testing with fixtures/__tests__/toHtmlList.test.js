70ff3b8ee66d6c0a4b0f3b31eca4ab08c01fe483
smilenb <alexander.is.dev@gmail.com>
2020-06-18 17:43:50 +0300

    refactor(21 advanced testing/03 testing with fixtures/__tests__/) toHtmlList.test.js

--- 21 advanced testing/03 testing with fixtures/__tests__/toHtmlList.test.js
+++ 21 advanced testing/03 testing with fixtures/__tests__/toHtmlList.test.js
@@ -5,21 +5,38 @@ import getFunction from '../functions.js';
 const toHtmlList = getFunction();
 
 // BEGIN (write your solution here)
+/* teacher solution */
+const formats = ['csv', 'json', 'yml'];
+const getFixturePath = (name) => path.join('__fixtures__', name);
+
 let expected;
-const getFilePath = (fileName) => (
-  path.join(`${path.resolve()}/21 advanced testing/03 testing with fixtures/__fixtures__/${fileName}`)
-);
-const filesNames = ['list.csv', 'list.json', 'list.yml'];
 
 beforeAll(async () => {
-  const resultContent = await fs.readFile(getFilePath('result.html'), 'utf-8');
-  expected = resultContent.toString().trim();
+  expected = await fs.readFile(getFixturePath('result.html'), 'utf-8');
 });
 
-test.each(filesNames.map((fileName) => getFilePath(fileName)))('toHtmlList(%s)', async (filePath) => {
-  const actual = await toHtmlList(filePath);
-  expect(actual).toEqual(expected);
+test.each(formats)('%s', async (format) => {
+  const filepath = getFixturePath(`list.${format}`);
+  const actual = await toHtmlList(filepath);
+  expected(actual).toEqual(expected.trim());
 });
+/* ************************************************ */
+
+// let expected;
+// const getFilePath = (fileName) => (
+//   path.join(`${path.resolve()}/21 advanced testing/03 testing with fixtures/__fixtures__/${fileName}`)
+// );
+// const filesNames = ['list.csv', 'list.json', 'list.yml'];
+
+// beforeAll(async () => {
+//   const resultContent = await fs.readFile(getFilePath('result.html'), 'utf-8');
+//   expected = resultContent.toString().trim();
+// });
+
+// test.each(filesNames.map((fileName) => getFilePath(fileName)))('toHtmlList(%s)', async (filePath) => {
+//   const actual = await toHtmlList(filePath);
+//   expect(actual).toEqual(expected);
+// });
 
 /* useful links */
 // dataset: https://ru.hexlet.io/topics/32043
