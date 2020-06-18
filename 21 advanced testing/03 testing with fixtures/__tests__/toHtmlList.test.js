0e691e2e3f8f553d699a196a9c192614f8940924
smilenb <alexander.is.dev@gmail.com>
2020-06-18 17:24:54 +0300

    refactor(21 advanced testing/03 testing with fixtures/__tests__/) toHtmlList.test.js

--- 21 advanced testing/03 testing with fixtures/__tests__/toHtmlList.test.js
+++ 21 advanced testing/03 testing with fixtures/__tests__/toHtmlList.test.js
@@ -5,29 +5,20 @@ import getFunction from '../functions.js';
 const toHtmlList = getFunction();
 
 // BEGIN (write your solution here)
-let normExpected;
+let expected;
+const getFilePath = (fileName) => (
+  path.join(`${path.resolve()}/21 advanced testing/03 testing with fixtures/__fixtures__/${fileName}`)
+);
+const filesNames = ['list.csv', 'list.json', 'list.yml'];
 
 beforeAll(async () => {
-  const expected = await fs.readFile(`${path.resolve()}/21 advanced testing/03 testing with fixtures/__fixtures__/result.html`, 'utf-8');
-  // 21 advanced testing/03 testing with fixtures/__fixtures__/list.csv
-  normExpected = expected.split('\n').map((str) => str.trim()).join('');
+  const resultContent = await fs.readFile(getFilePath('result.html'), 'utf-8');
+  expected = resultContent.toString().trim();
 });
 
-test('toHtmlList', async () => {
-  const getFixturePaths = (fileNames) => fileNames
-    .map((fileName) => path.join(path.resolve(), '21 advanced testing/03 testing with fixtures/__fixtures__/', fileName));
-
-  const filesNames = ['list.csv', 'list.json', 'list.yml'];
-  const paths = getFixturePaths(filesNames);
-
-  const fileContents = await Promise.all(paths.map((filePath) => toHtmlList(filePath)));
-  const normFileContents = fileContents
-    .map((fileContent) => fileContent.split('\n')
-      .map((str) => str.trim()).join(''));
-
-  expect(normFileContents[0]).toEqual(normExpected);
-  expect(normFileContents[1]).toEqual(normExpected);
-  expect(normFileContents[2]).toEqual(normExpected);
+test.each(filesNames.map((fileName) => getFilePath(fileName)))('toHtmlList(%s)', async (filePath) => {
+  const actual = await toHtmlList(filePath);
+  expect(actual).toEqual(expected);
 });
 
 /* useful links */
