36daa1de8611046bff8450a9798bedfec2dab24c
smilenb <alexander.is.dev@gmail.com>
2020-06-21 23:40:02 +0300

    start(21 advanced testing/) 06 inver deps/

---
+++ 21 advanced testing/06 inver deps/__tests__/getFilesCount.test.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import _ from 'lodash';
+import getFunction from '../functions.js';
+
+const getFilesCount = getFunction();
+
+const getFixturePath = (name) => path.join('__fixtures__', name);
+
+// BEGIN (write your solution here)
+
+
+// END
