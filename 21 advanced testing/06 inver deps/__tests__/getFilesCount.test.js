089f1c1893bc9574bd500c924e0799c4bece7a26
smilenb <alexander.is.dev@gmail.com>
2020-06-22 22:58:44 +0300

    add(21 advanced testing/06 inver deps/__tests__/) getFilesCount.test.js solution

--- 21 advanced testing/06 inver deps/__tests__/getFilesCount.test.js
+++ 21 advanced testing/06 inver deps/__tests__/getFilesCount.test.js
@@ -1,12 +1,22 @@
 import path from 'path';
 import _ from 'lodash';
-import getFunction from '../functions.js';
+import getFunction from '../functions';
 
 const getFilesCount = getFunction();
 
-const getFixturePath = (name) => path.join('__fixtures__', name);
+const getFixturePath = (name) => path.join('21 advanced testing/06 inver deps/__fixtures__', name);
 
 // BEGIN (write your solution here)
 
+test('get files count from flat', () => {
+  const pathToFlat = getFixturePath('flat');
+  const actual = getFilesCount(pathToFlat, _.noop);
+  expect(actual).toBe(3);
+});
 
+test('get files count from nested', () => {
+  const pathToFlat = getFixturePath('nested');
+  const actual = getFilesCount(pathToFlat, _.noop);
+  expect(actual).toBe(4);
+});
 // END
