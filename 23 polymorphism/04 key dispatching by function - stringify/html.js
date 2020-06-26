0059bec6b990b106c321950a19bdf7aa29b07cf1
smilenb <alexander.is.dev@gmail.com>
2020-06-27 00:14:54 +0300

    refactor(23 polymorphism/) 04 key dispatching by function - stringify/html.js

--- 23 polymorphism/04 key dispatching by function - stringify/html.js
+++ 23 polymorphism/04 key dispatching by function - stringify/html.js
@@ -24,38 +24,9 @@ const mapping = {
   },
 };
 
-const stringify = (tag) => {
+export default (tag) => {
   const build = mapping[tag.tagType];
   return build(tag);
 };
 
 // END
-// const hrTag = {
-//   name: 'hr',
-//   class: 'px-3',
-//   id: 'myid',
-//   tagType: 'single',
-// };
-// const html1 = stringify(hrTag); // <hr class="px-3" id="myid">
-// console.log(html1);
-
-// const divTag = {
-//   name: 'div',
-//   tagType: 'pair',
-//   body: 'text2',
-//   id: 'wow',
-// };
-// const html2 = stringify(divTag); // <div id="wow">text2</div>
-// console.log(html2);
-
-// const emptyDivTag = {
-//   name: 'div',
-//   tagType: 'pair',
-//   body: '',
-//   id: 'empty',
-// };
-
-// const html3 = stringify(emptyDivTag); // <div id="empty"></div>
-// console.log(html3);
-
-export default stringify;
