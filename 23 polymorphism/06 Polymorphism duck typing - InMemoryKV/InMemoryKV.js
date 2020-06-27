9ef50ff6c7e1b29212977a405deb2c844696035e
smilenb <alexander.is.dev@gmail.com>
2020-06-28 00:44:31 +0300

    refactor(23 polymorphism/) 06 Polymorphism duck typing - InMemoryKV/InMemoryKV.js

--- 23 polymorphism/06 Polymorphism duck typing - InMemoryKV/InMemoryKV.js
+++ 23 polymorphism/06 Polymorphism duck typing - InMemoryKV/InMemoryKV.js
@@ -3,24 +3,23 @@ import _ from 'lodash';
 // BEGIN (write your solution here)
 class inMemoryKV {
   constructor(initial = {}) {
-    Object.entries(initial)
-      .forEach(([key, value]) => this.set(key, value));
+    this.map = _.cloneDeep(initial);
   }
 
   set(key, value) {
-    this.initial = { ...this.initial, [key]: value };
+    this.map[key] = value;
   }
 
   unset(key) {
-    this.initial = _.omit(this.initial, key);
+    this.map = _.omit(this.map, key);
   }
 
   get(key, defaultValue = null) {
-    return _.get(this.initial, key, defaultValue);
+    return _.get(this.map, key, defaultValue);
   }
 
   toObject() {
-    return this.initial;
+    return this.map;
   }
 }
 
