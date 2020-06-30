6ff924ad54de945d0fc07e706edf8c91acd857e8
smilenb <alexander.is.dev@gmail.com>
2020-06-30 17:52:31 +0300

    refactor(23 polymorphism/09 dependency Inversion - Weather Service) /bin/wheather.js

--- 23 polymorphism/09 dependency Inversion - Weather Service/bin/wheather.js
+++ 23 polymorphism/09 dependency Inversion - Weather Service/bin/wheather.js
@@ -5,28 +5,12 @@ import WeatherService from '../WeatherService.js';
 
 // BEGIN (write your solution here)
 const city = process.argv[2];
-const client = axios;
+const httpClient = axios;
 
-const weatherService = new WeatherService(client);
+const weatherService = new WeatherService(httpClient);
 
 const temperature = weatherService.getTemperature(city);
-console.log(`Temperature in ${city}: ${temperature}C`);
-
-console.log(res);
+temperature.then((data) => {
+  console.log(`Temperature in ${city}: ${data}C`);
+});
 // END
-
-// const res = 'http://localhost:8080/api/v2/'
-//   .split('/')
-//   .filter((part) => part !== '');
-
-// const [protocol, host] = res;
-// console.log(protocol);
-// console.log(host);
-
-
-// import { resolve } from 'url';
-
-// const apiUrl = 'http://localhost:8080/api/v2/';
-// const city = 'berlin';
-// const request = resolve(apiUrl, city);
-// console.log(request);
