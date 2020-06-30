3e1bff59e65b04d74b4621e3e6eefa038ee2331e
smilenb <alexander.is.dev@gmail.com>
2020-06-30 17:51:38 +0300

    refactor(23 polymorphism/) 09 dependency Inversion - Weather Service/WeatherService.js

--- 23 polymorphism/09 dependency Inversion - Weather Service/WeatherService.js
+++ 23 polymorphism/09 dependency Inversion - Weather Service/WeatherService.js
@@ -4,34 +4,17 @@ const apiUrl = 'http://localhost:8080/api/v2/';
 
 // BEGIN (write your solution here)
 class WeatherService {
-  constructor(client) {
-    this.client = client;
+  constructor(httpClient) {
+    this.httpClient = httpClient;
   }
 
   async getTemperature(city) {
-    try {
-      const responce = await this.client.get(city, {
-        baseUrl: apiUrl
-      });
-      console.log(`########### ${responce} ############`);
-      return responce;
-    } catch (err) {
-      console.log(`########### ${err} ############`);
-      throw err;
-    }
+    const reqUrl = resolve(apiUrl, `cities/${city}`);
+    const responce = await this.httpClient.get(reqUrl);
+    const data = JSON.parse(responce.data);
+    return data.temperature;
   }
 }
 
 export default WeatherService;
 // END
-
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-/* test http request */
