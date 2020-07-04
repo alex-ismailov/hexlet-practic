901197b777d66ce1ad4cba216ece65fd52f7700d
smilenb <alexander.is.dev@gmail.com>
2020-07-04 21:57:47 +0300

    refactor(23.1 HTTP server/03 search in phonebook/) index.js - add teacher solution

--- 23.1 HTTP server/03 search in phonebook/index.js
+++ 23.1 HTTP server/03 search in phonebook/index.js
@@ -9,13 +9,21 @@ export default async (port, callback = () => {}) => {
   const data = await fs.readFile(path.join(path.resolve(), '23.1 HTTP server/03 search in phonebook/phonebook.txt'), 'utf-8');
 
   // BEGIN (write your solution here)
-  const userRows = data.trim().split('\n');
-  const usersById = userRows.reduce((acc, user) => {
-    const splited = user.split('|').map((item) => item.trim());
-    const [id, name, phone] = splited;
-    // return { ...acc, [id]: { name, phone } };
-    return _.set(acc, id, { name, phone });
-  }, {});
+  // const userRows = data.trim().split('\n');
+  // const usersById = userRows.reduce((acc, user) => {
+  //   const splited = user.split('|').map((item) => item.trim());
+  //   const [id, name, phone] = splited;
+  //   return { ...acc, [id]: { name, phone } };
+  // }, {});
+  
+  /* teacher solution */
+  const users = data.toString()
+    .trim()
+    .split('\n')
+    .map((value) => value.split('|').map((item) => item.trim()));
+  const usersIds = users.map(([id]) => id);
+  const usersData = users.map(([, name, phone]) => ({ name, phone }));
+  const usersById = _.zipObject(usersIds, usersData);
   // END
 
   const server = makeServer(usersById);
