d1fb1bcdc6556b657adeeb4b399c6a039a9e07bd
smilenb <alexander.is.dev@gmail.com>
2020-07-04 17:22:17 +0300

    refactor(23.1 HTTP server/03 search in phonebook/) server.js

--- 23.1 HTTP server/03 search in phonebook/server.js
+++ 23.1 HTTP server/03 search in phonebook/server.js
@@ -10,7 +10,26 @@ export default (usersById) => http.createServer((request, response) => {
       response.end(messages.join('\n'));
     } else if (request.url.startsWith('/search')) {
       // BEGIN (write your solution here)
-      
+      if (!request.url.includes('?')) {
+        response.end('');
+        return;
+      }
+      const url = new URL(request.url, `http://${request.headers.host}`);
+      const subStr = url.searchParams.get('q');
+
+      const keys = Object.keys(usersById);
+      const requiredUsers = keys.reduce((acc, key) => {
+        const userName = usersById[key].name.toLowerCase();
+        return userName.includes(subStr.toLowerCase())
+          ? [...acc, usersById[key]]
+          : acc;
+      }, []);
+
+      const resString = requiredUsers
+        .map((user) => `${user.name}, ${user.phone}`)
+        .join('\n');
+
+      response.end(resString);
       // END
     }
   });
