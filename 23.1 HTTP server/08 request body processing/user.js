e356c2ebe61ebbb1bf971ebf70e2507f66879aa4
smlnx <alexander.is.dev@gmail.com>
2020-07-08 01:08:24 +0300

    refactor(23.1 HTTP server/08 request body processing/user.js) pass tests

--- 23.1 HTTP server/08 request body processing/user.js
+++ 23.1 HTTP server/08 request body processing/user.js
@@ -7,6 +7,32 @@ export const nextId = () => {
 
 export const validate = ({ name, phone }) => {
   // BEGIN (write your solution here)
-  
+  const findErrors = (itemName, itemValue, regexp) => {
+    const errors = [];
+    if (!itemValue) {
+      const err = {
+        source: itemName,
+        title: 'can\'t be blank',
+      };
+      errors.push(err);
+      return errors;
+    }
+    if (!itemValue.match(regexp)) {
+      const err = {
+        source: itemName,
+        title: 'bad format',
+      };
+      errors.push(err);
+      return errors;
+    }
+    return errors;
+  };
+
+  const nameRegexp = /^[\w\.]+$/; // ^[\w]+[\w\. ]*$
+  const phoneRegexp = /^[\d]+[\d-]*$/;
+  const nameErrors = findErrors('name', name, nameRegexp);
+  const phoneErrors = findErrors('phone', phone, phoneRegexp);
+
+  return [...nameErrors, ...phoneErrors];
   // END
 };
