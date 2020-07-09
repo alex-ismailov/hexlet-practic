79c2730d76e97bbed0f181ee321f1f15499c065a
smlnx <alexander.is.dev@gmail.com>
2020-07-10 00:20:32 +0300

    add(23.2 Express/02 routes/solution.js) solution

--- 23.2 Express/02 routes/solution.js
+++ 23.2 Express/02 routes/solution.js
@@ -2,7 +2,32 @@ import Express from 'express';
 
 export default () => {
   // BEGIN (write your solution here)
-  
+  const app = new Express();
+
+  const counter = {
+    value: 0,
+  };
+
+  app.get('/', (req, res) => { // curl localhost:8080
+    res.json(counter);
+  });
+  app.post('/increment', (req, res) => { // curl --data increment "localhost:8080/increment"
+    counter.value += 1;
+    res.status(204).end();
+  });
+  app.post('/decrement', (req, res) => { // curl --data decrement "localhost:8080/decrement"
+    counter.value -= 1;
+    res.status(204).end();
+  });
+  app.delete('/reset', (req, res) => { // curl -X DELETE "localhost:8080/reset"
+    counter.value = 0;
+    res.status(204).end();
+  });
+  app.put('/set', (req, res) => {
+    const { value } = req.query;
+    counter.value = Number(value);
+    res.status(204).end();
+  });
   // END
 
   return app;
