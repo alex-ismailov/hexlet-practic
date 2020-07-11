f482d95bd75d9d3f6705fff018e1485ffc8f2a02
smlnx <alexander.is.dev@gmail.com>
2020-07-11 12:55:11 +0300

    implement(23.2 Express/06 templates - blog/solution.js)route /posts - handler

--- 23.2 Express/06 templates - blog/solution.js
+++ 23.2 Express/06 templates - blog/solution.js
@@ -3,11 +3,16 @@ import bodyParser from 'body-parser';
 
 import Post from './entities/Post.js';
 
+import path from 'path';
+
 export default () => {
   const app = new Express();
   app.set('view engine', 'pug');
   app.use('/assets', Express.static(process.env.NODE_PATH.split(':')[0]));
   app.use(bodyParser.urlencoded({ extended: false }));
+
+  /* local views directory location */
+  app.set('views', path.join(path.resolve(), '23.2 Express/06 templates - blog/views'));
 
   const posts = [
     new Post('hello', 'how are you?'),
@@ -19,7 +24,22 @@ export default () => {
   });
 
   // BEGIN (write your solution here)
-  
+  app.get('/posts', (req, res) => { // список постов
+    const data = { postsList: posts};
+    res.render('posts/index', data);
+  });
+
+  app.get('/posts/:id', (req, res) => { // страница поста
+
+  });
+
+  app.get('/posts/new', (req, res) => { // форма для создания нового поста
+    
+  });
+
+  app.post('/posts', (req, res) => { // форма для создания нового поста
+    
+  });
   // END
 
   return app;
