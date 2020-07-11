3cef7f469844b9b9cd92e2360e1d1886fa286ec7
smlnx <alexander.is.dev@gmail.com>
2020-07-11 12:53:46 +0300

    add(23.2 Express/06 templates - blog/bin/server.js) date time start console.log

--- 23.2 Express/06 templates - blog/bin/server.js
+++ 23.2 Express/06 templates - blog/bin/server.js
@@ -3,5 +3,5 @@ import solution from '../solution.js';
 const port = 8080;
 solution().listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server was started on '${port}'`);
+  console.log(`Server was started on '${port}', at ${new Date().toLocaleString()}`);
 });
