f2596ddb04664e54cf3db6a7a9029cc0b4104ae0
smlnx <alexander.is.dev@gmail.com>
2020-07-11 12:52:42 +0300

    implement(23.2 Express/06 templates - blog/entities/) Post.js

--- 23.2 Express/06 templates - blog/entities/Post.js
+++ 23.2 Express/06 templates - blog/entities/Post.js
@@ -1,3 +1,16 @@
 // BEGIN (write your solution here)
+let id = 0;
+const getId = () => {
+  const currentId = id;
+  id += 1;
+  return currentId;
+};
 
+export default class Post {
+  constructor(title, body) {
+    this.id = getId();
+    this.title = title;
+    this.body = body;
+  }
+}
 // END
