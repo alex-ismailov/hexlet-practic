8d00a4acf8a269b06cd8f3dcac9a109030ed5757
smlnx <alexander.is.dev@gmail.com>
2020-07-12 12:46:29 +0300

    refactor(23.2 Express/08 REST - blog editable post/bin/server.js) add date-time info

--- 23.2 Express/08 REST - blog editable post/bin/server.js
+++ 23.2 Express/08 REST - blog editable post/bin/server.js
@@ -3,5 +3,5 @@ import runServer from '../application.js';
 const port = 8080;
 runServer().listen(port, () => {
   // eslint-disable-next-line
-  console.log('Server has been started')
+  console.log(`Server has been started, at ${new Date()}`)
 });
