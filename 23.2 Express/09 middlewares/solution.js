9d733511a92080d0cc9bdcf51a113086499f719f
smlnx <alexander.is.dev@gmail.com>
2020-07-14 22:48:40 +0300

    refactor(23.2 Express/09 middlewares/) solution.js

--- 23.2 Express/09 middlewares/solution.js
+++ 23.2 Express/09 middlewares/solution.js
@@ -2,8 +2,7 @@ import UAParser from 'ua-parser-js';
 
 // BEGIN (write your solution here)
 export default (req, res, next) => {
-  const parser = new UAParser(req.headers['user-agent']);
-  req.useragent = parser.getResult();
+  req.useragent = UAParser(req.headers['user-agent']);
   next();
 };
 // END
