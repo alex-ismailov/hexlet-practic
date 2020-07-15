3eb1a1640bd123a29677b0fbc1ea4280e7f74b35
smlnx <alexander.is.dev@gmail.com>
2020-07-15 22:45:55 +0300

    implement(23.2 Express/11 sessions/entities/) User.js

--- 23.2 Express/11 sessions/entities/User.js
+++ 23.2 Express/11 sessions/entities/User.js
@@ -1,3 +1,15 @@
 // BEGIN (write your solution here)
+export default class User {
+  constructor(name, password) {
+    this.nickname = name;
+    this.password = password;
+    this.guest = false;
+  }
 
+  isGuest() {
+    return this.guest;
+  }
+}
 // END
+
+
