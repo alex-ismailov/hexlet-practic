8e92d35732a74e641ed40bb9eed2ae643e5650d4
smlnx <alexander.is.dev@gmail.com>
2020-07-16 23:37:51 +0300

    implement(23.2 Express/13 flash/) flash.js

--- 23.2 Express/13 flash/flash.js
+++ 23.2 Express/13 flash/flash.js
@@ -1,3 +1,15 @@
 // BEGIN (write your solution here)
+import assert from 'assert';
 
+export default () => (req, res, next) => {
+  assert(req.session, 'a req.session is required!');
+  res.locals.flash = req.session.flash || [];
+  res.flash = (type, message) => {
+    const messages = res.locals.flash;
+    messages.push({ type, message });
+    req.session.flash = messages;
+  };
+
+  next();
+};
 // END
