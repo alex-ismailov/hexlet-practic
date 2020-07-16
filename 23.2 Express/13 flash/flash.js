68f54705b9894cae49e1d52620945a197cafd050
smlnx <alexander.is.dev@gmail.com>
2020-07-17 00:56:09 +0300

    refactor(23.2 Express/13 flash/) flash.js

--- 23.2 Express/13 flash/flash.js
+++ 23.2 Express/13 flash/flash.js
@@ -4,12 +4,10 @@ import assert from 'assert';
 export default () => (req, res, next) => {
   assert(req.session, 'a req.session is required!');
   res.locals.flash = req.session.flash || [];
+  req.session.flash = [];
   res.flash = (type, message) => {
-    const messages = res.locals.flash;
-    messages.push({ type, message });
-    req.session.flash = messages;
+    req.session.flash.push({ type, message });
   };
-
   next();
 };
 // END
