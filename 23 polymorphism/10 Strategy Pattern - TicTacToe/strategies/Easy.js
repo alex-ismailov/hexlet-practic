565f946f627605c1f826b3553cf52b550f147f1b
smlnx <alexander.is.dev@gmail.com>
2020-07-18 22:04:04 +0300

    implement(23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/) Easy.js

--- 23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/Easy.js
+++ 23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/Easy.js
@@ -2,7 +2,16 @@
 
 class Easy {
   // BEGIN (write your solution here)
-  
+  getCoordinates(gameField) {
+    for (let row = 0; row < gameField.length; row += 1) {
+      for (let cell = 0; cell < gameField[row].length; cell += 1) {
+        if (gameField[row][cell] === null) {
+          return [row, cell];
+        }
+      }
+    }
+    return false;
+  }
   // END
 }
 
