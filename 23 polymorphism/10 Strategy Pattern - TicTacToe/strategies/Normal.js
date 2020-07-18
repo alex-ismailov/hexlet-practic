d1fed9c8359785fd9b27d4090ff9091a213e7e50
smlnx <alexander.is.dev@gmail.com>
2020-07-18 22:04:39 +0300

    iplement(23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/) Normal.js

--- 23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/Normal.js
+++ 23 polymorphism/10 Strategy Pattern - TicTacToe/strategies/Normal.js
@@ -2,7 +2,16 @@
 
 class Normal {
   // BEGIN (write your solution here)
-  
+  getCoordinates(gameField) {
+    for (let row = gameField.length - 1; row >= 0; row -= 1) {
+      for (let cell = 0; cell < gameField[row].length; cell += 1) {
+        if (gameField[row][cell] === null) {
+          return [row, cell];
+        }
+      }
+    }
+    return false;
+  }
   // END
 }
 
