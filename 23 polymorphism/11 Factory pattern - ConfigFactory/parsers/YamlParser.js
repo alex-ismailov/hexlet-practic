9c2e32e0c4d56fbcf4c806675553c77608addcd9
smlnx <alexander.is.dev@gmail.com>
2020-07-19 19:32:57 +0300

    implement(23 polymorphism/11 Factory pattern - ConfigFactory/parsers/) YamlParser.js

--- 23 polymorphism/11 Factory pattern - ConfigFactory/parsers/YamlParser.js
+++ 23 polymorphism/11 Factory pattern - ConfigFactory/parsers/YamlParser.js
@@ -1,5 +1,11 @@
+/* eslint-disable class-methods-use-this */
+
 import yaml from 'js-yaml';
 
 // BEGIN (write your solution here)
-
+export default class YamlParser {
+  parse(str) {
+    return yaml.safeLoad(str);
+  }
+}
 // END
