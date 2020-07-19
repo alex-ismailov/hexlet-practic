8415bed959ad397e8158651cfdae330a546cf566
smlnx <alexander.is.dev@gmail.com>
2020-07-19 22:54:38 +0300

    implement(23 polymorphism/12 design patterns - LabelTag wrapper/tags/) LabelTag.js

--- 23 polymorphism/12 design patterns - LabelTag wrapper/tags/LabelTag.js
+++ 23 polymorphism/12 design patterns - LabelTag wrapper/tags/LabelTag.js
@@ -1,3 +1,16 @@
 // BEGIN (write your solution here)
+export default class LabelTag {
+  constructor(text, innerTag) {
+    this.text = text;
+    this.innerTag = innerTag;
+  }
 
+  render() {
+    return `<label>${this.text}${this.innerTag.render()}</label>`;
+  }
+
+  toString() {
+    return this.render();
+  }
+}
 // END
