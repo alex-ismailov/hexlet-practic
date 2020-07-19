b599a6e252af3d427eb3d5c1b1919d49b7b2584a
smlnx <alexander.is.dev@gmail.com>
2020-07-19 19:32:00 +0300

    implement(23 polymorphism/11 Factory pattern - ConfigFactory/) ConfigFactory.js

--- 23 polymorphism/11 Factory pattern - ConfigFactory/ConfigFactory.js
+++ 23 polymorphism/11 Factory pattern - ConfigFactory/ConfigFactory.js
@@ -5,5 +5,20 @@ import YamlParser from './parsers/YamlParser.js';
 import Config from './Config.js';
 
 // BEGIN (write your solution here)
+// 1. принимает на вход путь до файла конфигурации в формате либо json либо yaml
+export default class ConfigFactory {
+  static parsers = {
+    json: JsonParser,
+    yaml: YamlParser,
+    yml: YamlParser,
+  };
 
+  static factory(filepath) {
+    const fileExt = path.extname(filepath).split('.')[1];
+    const parser = new ConfigFactory.parsers[fileExt]();
+    const content = fs.readFileSync(filepath);
+    const parsedContent = parser.parse(content);
+    return new Config(parsedContent);
+  }
+}
 // END
