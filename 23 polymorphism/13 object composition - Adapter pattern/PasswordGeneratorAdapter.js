8fc25cb56396fca2459dc60b1966e3e217ed24a7
smlnx <alexander.is.dev@gmail.com>
2020-07-20 17:02:47 +0300

    implement(23 polymorphism/13 object composition - Adapter pattern/) PasswordGeneratorAdapter.js

--- 23 polymorphism/13 object composition - Adapter pattern/PasswordGeneratorAdapter.js
+++ 23 polymorphism/13 object composition - Adapter pattern/PasswordGeneratorAdapter.js
@@ -2,6 +2,17 @@ import generator from 'generate-password';
 
 export default class PasswordGeneratorAdapter {
   // BEGIN (write your solution here)
-  
+  options = {
+    uppercase: false,
+    numbers: false,
+    symbols: false,
+  };
+
+  generatePassword(length, options) {
+    const normalizedOptions = options
+      .reduce((acc, option) => ({ ...acc, [option]: true }), { length, ...this.options });
+
+    return generator.generate(normalizedOptions);
+  }
   // END
 }
