4212ed9068caeb7a4eb39b446dd0f4c7d3c6c4f8
smlnx <alexander.is.dev@gmail.com>
2020-07-22 01:27:40 +0300

    implement(23 polymorphism/14 State pattern - TcpConnection/states/) Connected.js

--- 23 polymorphism/14 State pattern - TcpConnection/states/Connected.js
+++ 23 polymorphism/14 State pattern - TcpConnection/states/Connected.js
@@ -2,6 +2,25 @@
 
 export default class Connected {
   // BEGIN (write your solution here)
-  
+  constructor(tcpConnection) {
+    this.tcpConnection = tcpConnection;
+  }
+
+  connect() {
+    throw new Error('Connection already established');
+  }
+
+  getCurrentState() {
+    return 'connected';
+  }
+
+  write(data) {
+    this.tcpConnection.data = data;
+  }
+
+  disconnect() {
+    this.tcpConnection.state = new this.tcpConnection.states.Disconnected(this.tcpConnection);
+  }
+
   // END
 }
