435865981c1c71457bd0ad37767b8196e40abcab
smlnx <alexander.is.dev@gmail.com>
2020-07-22 01:28:13 +0300

    implement(23 polymorphism/14 State pattern - TcpConnection/states/) Disconnected.js

--- 23 polymorphism/14 State pattern - TcpConnection/states/Disconnected.js
+++ 23 polymorphism/14 State pattern - TcpConnection/states/Disconnected.js
@@ -2,6 +2,24 @@
 
 export default class Disconnected {
   // BEGIN (write your solution here)
-  
+  constructor(tcpConnection) {
+    this.tcpConnection = tcpConnection;
+  }
+
+  connect() {
+    this.tcpConnection.state = new this.tcpConnection.states.Connected(this.tcpConnection);
+  }
+
+  getCurrentState() {
+    return 'disconnected';
+  }
+
+  write() {
+    throw new Error('You cannot write connection disconnected');
+  }
+
+  disconnect() {
+    throw new Error('You cannot disconnect the connection is disconnected already');
+  }
   // END
 }
