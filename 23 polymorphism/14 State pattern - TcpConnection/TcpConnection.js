a552403a45b58947c736370c2b1362552ff6eeeb
smlnx <alexander.is.dev@gmail.com>
2020-07-22 01:27:14 +0300

    implement(23 polymorphism/14 State pattern - TcpConnection/) TcpConnection.js

--- 23 polymorphism/14 State pattern - TcpConnection/TcpConnection.js
+++ 23 polymorphism/14 State pattern - TcpConnection/TcpConnection.js
@@ -3,6 +3,31 @@ import ConnectedState from './states/Connected.js';
 
 export default class TcpConnection {
   // BEGIN (write your solution here)
-  
+  constructor(ip, port) {
+    this.ip = ip;
+    this.port = port;
+    this.states = {
+      Disconnected: DisconnectedState,
+      Connected: ConnectedState,
+    };
+    this.state = new this.states.Disconnected(this);
+    this.data = '';
+  }
+
+  connect() {
+    this.state.connect();
+  }
+
+  getCurrentState() {
+    return this.state.getCurrentState();
+  }
+
+  write(data) {
+    this.state.write(data);
+  }
+
+  disconnect() {
+    this.state.disconnect();
+  }
   // END
 }
