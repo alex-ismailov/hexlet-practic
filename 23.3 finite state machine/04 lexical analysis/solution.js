07285b4a585c827a9b41cce2a6737f82dcc71430
smlnx <alexander.is.dev@gmail.com>
2020-07-21 12:01:31 +0300

    refactor(23.3 finite state machine/04 lexical analysis/) solution.js

--- 23.3 finite state machine/04 lexical analysis/solution.js
+++ 23.3 finite state machine/04 lexical analysis/solution.js
@@ -1,5 +1,41 @@
 // BEGIN (write your solution here)
 export default (text) => {
-  
+  let word = [];
+  const result = [];
+  let state = 'beforeFirstWord'; // insideFirstWord, after
+
+  for (let i = 0; i < text.length; i += 1) {
+    const symbol = text[i];
+    switch (state) {
+      case 'beforeFirstWord':
+        if (symbol !== ' ' && symbol !== '\n') {
+          state = 'insideFirstWord';
+          word.push(symbol);
+        }
+        break;
+      case 'insideFirstWord':
+        if (symbol === ' ' || symbol === '\n') {
+          result.push(word.join(''));
+          word = [];
+          state = symbol === ' ' ? 'afterFirstWord' : 'beforeFirstWord';
+        } else {
+          word.push(symbol);
+        }
+        break;
+      case 'afterFirstWord':
+        if (symbol === '\n') {
+          state = 'beforeFirstWord';
+        }
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
+
+  if (word.length > 0) {
+    result.push(word.join(''));
+  }
+
+  return result;
 };
 // END
