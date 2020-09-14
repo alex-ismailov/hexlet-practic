e99fc9131014680fd8b81dacf9ed557e361b541f
smlnx <alexander.is.dev@gmail.com>
2020-09-15 00:58:21 +0300

    fix(09 objects/03 obj props modification) eslint error

--- 09 objects/03 obj props modification/lesson.js
+++ 09 objects/03 obj props modification/lesson.js
@@ -5,6 +5,6 @@ import _ from 'lodash';
 export default (obj) => {
   const [name, description] = Object.keys(obj);
   obj[name] = _.capitalize(obj[name]);
-  obj[description] = obj[description].toLowerCase(); 
+  obj[description] = obj[description].toLowerCase();
 };
 // END
