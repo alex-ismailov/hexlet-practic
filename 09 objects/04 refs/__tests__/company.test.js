ca0da4d3d310718d399f308f9a3c851d9d3f83a5
smlnx <alexander.is.dev@gmail.com>
2020-09-15 02:30:35 +0300

    fix(09 objects/04 refs/__tests__/company.test.js) eslint errors

--- 09 objects/04 refs/__tests__/company.test.js
+++ 09 objects/04 refs/__tests__/company.test.js
@@ -32,7 +32,7 @@ it('is 5', () => {
 
 it('is 6 deep equal', () => {
   const innerCompany = { name: 'InnerHexlet', website: 'https://innerHexlet.io' };
-  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany};
+  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany };
   const company2 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany };
   expect(is(company1, company2)).toBe(true);
 });
@@ -40,7 +40,7 @@ it('is 6 deep equal', () => {
 it('is 7 deep equal', () => {
   const innerCompany1 = { name: 'InnerHexlet', website: 'https://innerHexlet.io' };
   const innerCompany2 = { name: 'InnerHexlet-2', website: 'https://innerHexlet-2.io' };
-  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany1};
+  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany1 };
   const company2 = { name: 'Hexlet', website: 'https://code-basics.com', innerCompany: innerCompany2 };
   expect(is(company1, company2)).toBe(false);
 });
