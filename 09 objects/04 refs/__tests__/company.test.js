e58b4c1663821540e07d119e2f9f729598e16f1b
smlnx <alexander.is.dev@gmail.com>
2020-09-15 02:27:52 +0300

    add(09 objects/04 refs/__tests__/company.test.js) deep equal tests

--- 09 objects/04 refs/__tests__/company.test.js
+++ 09 objects/04 refs/__tests__/company.test.js
@@ -23,3 +23,24 @@ it('is 4', () => {
   const company2 = { name: 'Hexlet', website: 'https://code-basics.com' };
   expect(is(company1, company2)).toBe(false);
 });
+
+it('is 5', () => {
+  const company1 = { name: 'Hexlet', website: 'https://hexlet.io' };
+  const company2 = { name: 'Hexlet', website: 'https://code-basics.com', innerCompany: 'company1' };
+  expect(is(company1, company2)).toBe(false);
+});
+
+it('is 6 deep equal', () => {
+  const innerCompany = { name: 'InnerHexlet', website: 'https://innerHexlet.io' };
+  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany};
+  const company2 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany };
+  expect(is(company1, company2)).toBe(true);
+});
+
+it('is 7 deep equal', () => {
+  const innerCompany1 = { name: 'InnerHexlet', website: 'https://innerHexlet.io' };
+  const innerCompany2 = { name: 'InnerHexlet-2', website: 'https://innerHexlet-2.io' };
+  const company1 = { name: 'Hexlet', website: 'https://hexlet.io', innerCompany: innerCompany1};
+  const company2 = { name: 'Hexlet', website: 'https://code-basics.com', innerCompany: innerCompany2 };
+  expect(is(company1, company2)).toBe(false);
+});
