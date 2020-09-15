4fa8d6da12eb7fc423d27a688d283cbabe85f9f1
smlnx <alexander.is.dev@gmail.com>
2020-09-16 02:18:51 +0300

    refactor(09 objects/09 object merge/objects.js) like teacher solution

--- 09 objects/09 object merge/objects.js
+++ 09 objects/09 object merge/objects.js
@@ -2,11 +2,9 @@ import _ from 'lodash';
 
 // BEGIN (write your solution here)
 export default (target, keys, source) => {
-  if (keys.length > 0) {
-    const newSource = _.pick(source, keys);
-    Object.assign(target, newSource);
-  } else {
-    Object.assign(target, source);
-  }
+  const filteredData = keys.length > 0
+    ? _.pick(source, keys)
+    : source;
+  Object.assign(target, filteredData);
 };
 // END
