73eb015869a5a932e744249bbb1e45d05730f012
smilenb <alexander.is.dev@gmail.com>
2020-09-15 15:17:12 +0300

    refactor(09 objects/08 nested objects - pickDeep props/objects.js) implementation with for of

--- 09 objects/08 nested objects - pickDeep props/objects.js
+++ 09 objects/08 nested objects - pickDeep props/objects.js
@@ -1,22 +1,21 @@
 /* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
 
 // BEGIN (write your solution here)
-// export default (data, keys) => {
-//   let acc = data;
-//   for (const key of keys) {
-//     if (Object.prototype.hasOwnProperty.call(acc, key)) {
-//       acc = acc[key];
-//     } else {
-//       return null;
-//     }
-//   }
-//   return acc;
-// };
+export default (data, keys) => {
+  let acc = data;
+  for (const key of keys) {
+    if (!Object.prototype.hasOwnProperty.call(acc, key)) {
+      return null;
+    }
+    acc = acc[key];
+  }
+  return acc;
+};
 
 // Решение Куратора 0xD34F, с Хабр Q&A, link: https://qna.habr.com/q/701487
-export default (data, keys) => keys.reduce((acc, key) => (
-  acc && Object.prototype.hasOwnProperty.call(acc, key) 
-    ? acc[key]
-    : null
-), data);
+// export default (data, keys) => keys.reduce((acc, key) => (
+//   acc && Object.prototype.hasOwnProperty.call(acc, key) 
+//     ? acc[key]
+//     : null
+// ), data);
 // END
