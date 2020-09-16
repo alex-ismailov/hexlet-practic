26f1a8cb59061dfd0b682f7e6fbb51d9b5e4fb23
smlnx <alexander.is.dev@gmail.com>
2020-09-16 23:51:22 +0300

    refactor(09 objects/10 cloning/) objects.js

--- 09 objects/10 cloning/objects.js
+++ 09 objects/10 cloning/objects.js
@@ -10,7 +10,7 @@ const cloneDeep = (data) => {
   for (const key of keys) {
     const dataProp = data[key];
     result[key] = _.isObject(dataProp)
-      ? { ...cloneDeep(dataProp) }
+      ? cloneDeep(dataProp)
       : dataProp;
   }
 
