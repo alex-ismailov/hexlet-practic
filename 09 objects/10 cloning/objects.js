9af575ce9853880940c9ed17947b0eccf3e1d459
smlnx <alexander.is.dev@gmail.com>
2020-09-16 23:03:56 +0300

    refactor(09 objects/10 cloning/) objects.js

--- 09 objects/10 cloning/objects.js
+++ 09 objects/10 cloning/objects.js
@@ -5,11 +5,7 @@ import _ from 'lodash';
 // BEGIN (write your solution here)
 const cloneDeep = (data) => {
   const result = {};
-
   const keys = Object.keys(data);
-  if (keys.length === 0) {
-    return result;
-  }
 
   for (const key of keys) {
     const dataProp = data[key];
