ec1731b364d9442db823f4ff74d38109a8beb896
smlnx <alexander.is.dev@gmail.com>
2020-09-16 23:55:39 +0300

    refactor(09 objects/10 cloning/) objects.js

--- 09 objects/10 cloning/objects.js
+++ 09 objects/10 cloning/objects.js
@@ -3,19 +3,24 @@
 import _ from 'lodash';
 
 // BEGIN (write your solution here)
-const cloneDeep = (data) => {
-  const result = {};
-  const keys = Object.keys(data);
+// const cloneDeep = (data) => {
+//   const result = {};
+//   const keys = Object.keys(data);
 
-  for (const key of keys) {
-    const dataProp = data[key];
-    result[key] = _.isObject(dataProp)
-      ? cloneDeep(dataProp)
-      : dataProp;
-  }
+//   for (const key of keys) {
+//     const dataProp = data[key];
+//     result[key] = _.isObject(dataProp)
+//       ? cloneDeep(dataProp)
+//       : dataProp;
+//   }
 
-  return result;
-};
+//   return result;
+// };
+
+const cloneDeep = (data) => Object.entries(data)
+  .reduce((acc, [key, value]) => _.isObject(value)
+    ? { ...acc, [key]: cloneDeep(value) }
+    : { ...acc, [key]: value }, {});
 
 export default cloneDeep;
 // END
