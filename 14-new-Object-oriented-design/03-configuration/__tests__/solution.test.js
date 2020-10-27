0bbcd5b68d41806ca73e13dda3a2a4cd0abab320
smlnx <alexander.is.dev@gmail.com>
2020-10-28 00:11:29 +0300

    add(14-new-Object-oriented-design/) 03-configuration

---
+++ 14-new-Object-oriented-design/03-configuration/__tests__/solution.test.js
@@ -0,0 +1,54 @@
+import PasswordValidator from '../solution.js';
+
+test('testValidateWithDefaultOptions', () => {
+  const validator = new PasswordValidator();
+  const errors1 = validator.validate('qwertyui');
+  expect(errors1).toEqual({});
+
+  const errors2 = validator.validate('qwerty');
+  expect(errors2).toEqual({ minLength: 'too small' });
+
+  const errors3 = validator.validate('another-password');
+  expect(errors3).toEqual({});
+});
+
+test('testValidateWithOptions 1', () => {
+  const validator = new PasswordValidator({ containNumbers: true });
+  const errors1 = validator.validate('qwertya3sdf');
+  expect(errors1).toEqual({});
+
+  const errors2 = validator.validate('qwerty');
+  expect(errors2).toEqual({
+    minLength: 'too small',
+    containNumbers: 'should contain at least one number',
+  });
+
+  const errors3 = validator.validate('q23ty');
+  expect(errors3).toEqual({ minLength: 'too small' });
+});
+
+test('testValidateWithOptions 2', () => {
+  const validator = new PasswordValidator({ containNumbers: true, minLength: 10 });
+  const errors1 = validator.validate('qwert3yag');
+  expect(errors1).toEqual({
+    minLength: 'too small',
+  });
+
+  const errors2 = validator.validate('qwerty');
+  expect(errors2).toEqual({
+    minLength: 'too small',
+    containNumbers: 'should contain at least one number',
+  });
+
+  const errors3 = validator.validate('q2wer3ty4i');
+  expect(errors3).toEqual({});
+});
+
+test('testValidateWithIncorrectOptions', () => {
+  const validator = new PasswordValidator({ containNumberz: null });
+  const errors1 = validator.validate('qwertyui');
+  expect(errors1).toEqual({});
+
+  const errors2 = validator.validate('qwerty');
+  expect(errors2).toEqual({ minLength: 'too small' });
+});
---
+++ 14-new-Object-oriented-design/03-configuration/solution.js
@@ -0,0 +1,25 @@
+const hasNumber = (string) => (string.search(/\d/) !== -1);
+
+// BEGIN (write your solution here)
+export default class PasswordValidator {
+  constructor(config = {}) {
+    const { minLength = 8, containNumbers = false } = config;
+    this.minLength = minLength;
+    this.containNumbers = containNumbers;
+  }
+
+  validate(pass) {
+    const errors = {};
+
+    if (`${pass}`.length < this.minLength) {
+      errors.minLength = 'too small';
+    }
+
+    if (this.containNumbers !== hasNumber(pass)) {
+      errors.containNumbers = 'should contain at least one number';
+    }
+
+    return errors;
+  }
+}
+// END
