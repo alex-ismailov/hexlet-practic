28033b15ee772d72ee3ba7b32ccff3940f25f5e5
smlnx <alexander.is.dev@gmail.com>
2020-10-29 09:21:08 +0300

    refactor(14-new-Object-oriented-design/04-modifiable-configuration/solution.js) not pass all tests

--- 14-new-Object-oriented-design/04-modifiable-configuration/solution.js
+++ 14-new-Object-oriented-design/04-modifiable-configuration/solution.js
@@ -5,6 +5,22 @@ export default class Truncater {
   };
 
   // BEGIN (write your solution here)
-  
+  constructor(options = {}) {
+    this.options = { ...Truncater.defaultOptions, ...options };
+  };
+
+  truncate(str, newOptions = {}) {
+    console.log(this.options)
+    const currentOptions = { ...this.options, ...newOptions };
+    const { separator, length } = currentOptions;
+
+    return `${str.slice(0, length)}${separator}`;
+  }
   // END
 }
+
+// =============
+
+const sep = new Truncater();
+console.log(sep.truncate('one two'));
+// console.log(sep.truncate('one two', { length: 6 }));
