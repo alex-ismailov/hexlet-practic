83eef928299b62bc225c8941a51320e91eb4c299
smilenb <alexander.is.dev@gmail.com>
2020-10-29 19:37:42 +0300

    add(14-new-Object-oriented-design/04-modifiable-configuration/solution.js) ex. solution

--- 14-new-Object-oriented-design/04-modifiable-configuration/solution.js
+++ 14-new-Object-oriented-design/04-modifiable-configuration/solution.js
@@ -7,20 +7,15 @@ export default class Truncater {
   // BEGIN (write your solution here)
   constructor(options = {}) {
     this.options = { ...Truncater.defaultOptions, ...options };
-  };
+  }
 
   truncate(str, newOptions = {}) {
-    console.log(this.options)
     const currentOptions = { ...this.options, ...newOptions };
     const { separator, length } = currentOptions;
 
-    return `${str.slice(0, length)}${separator}`;
+    return length < str.length
+      ? `${str.slice(0, length)}${separator}`
+      : str;
   }
   // END
 }
-
-// =============
-
-const sep = new Truncater();
-console.log(sep.truncate('one two'));
-// console.log(sep.truncate('one two', { length: 6 }));
