e5fe5851fbb0ec945d731029fba3c9cdfd8f9081
smlnx <alexander.is.dev@gmail.com>
2020-10-30 18:25:33 +0300

    add(14-new-Object-oriented-design/06-fluent-Interface/solution.js) ex. solution

--- 14-new-Object-oriented-design/06-fluent-Interface/solution.js
+++ 14-new-Object-oriented-design/06-fluent-Interface/solution.js
@@ -1,62 +1,26 @@
 /* нормализует их имена, сортирует и группирует по стране. */
 
 // BEGIN (write your solution here)
-// const normalize = (cities) => cities
-//   .map((city) => {
-//     const { name, country } = city;
-//     return { name: name.toLowerCase().trim(), country: country.toLowerCase().trim() };
-//   })
-//   .sort((a, b) => {
-//     if (a.country > b.country) {
-//       return 1;
-//     }
-//     if (a.country < b.country) {
-//       return -1;
-//     }
-//     return 0;
-//   })
-//   .reduce((acc, { name, country }) => {
-//     return { ...acc, [country]: [ ...acc[country] ?? [], name].sort() };
-//   }, {});
+const compareString = (a, b) => {
+  if (a.country > b.country) {
+    return 1;
+  }
+  if (a.country < b.country) {
+    return -1;
+  }
+  return 0;
+};
 
 const normalize = (cities) => cities
   .map(({ name, country }) => (
-    { name: name.toLowerCase().trim(), country: country.toLowerCase().trim() })
-  )
-  .sort((a, b) => {
-    if (a.country > b.country) {
-      return 1;
-    }
-    if (a.country < b.country) {
-      return -1;
-    }
-    return 0;
-  })
+    { name: name.toLowerCase().trim(), country: country.toLowerCase().trim() }))
+  .sort(compareString)
   .reduce((acc, { name, country }) => {
-    return { ...acc, [country]: [ ...acc[country] ?? [], name].sort() };
-  }, {})
-  
-
+    const citiesAcc = [...acc[country] ?? [], name];
+    const citiesAccFiltered = citiesAcc
+      .filter((elem, currentIndex, arr) => arr.indexOf(elem) === currentIndex);
+    return { ...acc, [country]: citiesAccFiltered.sort() };
+  }, {});
 
 export default normalize;
 // END
-
-
-const countries = [
-  { name: 'istambul', country: 'turkey' },
-  { name: 'Moscow ', country: ' Russia' },
-  { name: 'iStambul', country: 'tUrkey' },
-  { name: 'antalia', country: 'turkeY ' },
-  { name: 'samarA', country: '  ruSsiA' },
-  { name: 'Miami', country: 'usa' },
-];
-console.log(normalize(countries));
-// {
-//   russia: [
-//     'moscow',
-//     'samara',
-//   ],
-//   usa: [
-//     'miami',
-//   ],
-// }
