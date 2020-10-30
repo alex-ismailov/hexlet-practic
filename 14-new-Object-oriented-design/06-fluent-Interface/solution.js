e1bb0bb4326092fa83a34208c5f081f8e3ac6771
smlnx <alexander.is.dev@gmail.com>
2020-10-30 22:01:03 +0300

    add(14-new-Object-oriented-design/06-fluent-Interface/) solution.js - teacher solution

--- 14-new-Object-oriented-design/06-fluent-Interface/solution.js
+++ 14-new-Object-oriented-design/06-fluent-Interface/solution.js
@@ -22,5 +22,19 @@ const normalize = (cities) => cities
     return { ...acc, [country]: citiesAccFiltered.sort() };
   }, {});
 
+/* teacher solution */
+// export default (data) => data
+//   .map(({ name, country }) => ({ city: name.toLowerCase(), country: country.toLowerCase() }))
+//   .map(({ city, country }) => ({ city: city.trim(), country: country.trim() }))
+//   .map(({ city, country }) => [country, city])
+//   .sort() // sort countries and cities
+//   .reduce((acc, [country, city]) => {
+//     const citiesAcc = acc[country] ?? [];
+//     const cities = citiesAcc.concat(city);
+//     const uniqueCities = new Set(cities);
+//     return { ...acc, [country]: [...uniqueCities] };
+//   }, {});
+// *******************************
+
 export default normalize;
 // END
