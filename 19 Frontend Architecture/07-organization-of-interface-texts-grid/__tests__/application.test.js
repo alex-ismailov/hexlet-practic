69c093b1bc3ec33e2afbd6e6d90dafb301611490
smilenb <alexander.is.dev@gmail.com>
2020-11-07 15:08:42 +0300

    init(19 Frontend Architecture/) 07-organization-of-interface-texts-grid/ - ex.

---
+++ 19 Frontend Architecture/07-organization-of-interface-texts-grid/__tests__/application.test.js
@@ -0,0 +1,66 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+// import testingLibrary from '@testing-library/dom';
+import userEventHelpers from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+import run from '../src/application.js';
+
+// const within = testingLibrary.within;
+import { within } from '@testing-library/dom';
+import { getNodeText } from '@testing-library/dom';
+// const getNodeText = testingLibrary.getNodeText;
+// const userEvent = userEventHelpers.default;
+
+const getFixture = (filename) => fs.readFileSync(path.join('__fixtures__', filename)).toString();
+const isEven = (int) => int % 2 === 0;
+const toPairs = (array) => array.reduce((acc, item, idx, arr) => isEven(idx) ? acc : [...acc, [arr[idx - 1], item]], []);
+const getProps = (queries) => {
+  const rawProps = queries.getAllByRole('cell')
+    .map((cell) => getNodeText(cell));
+  const props = toPairs(rawProps);
+
+  return props;
+};
+
+beforeEach(async () => {
+  const initHtml = getFixture('index.html');
+  document.documentElement.innerHTML = initHtml;
+  await run();
+});
+
+test('application', async () => {
+  const queries = within(document.querySelector('.container'));
+  const expectedProps = JSON.parse(getFixture('props.json'));
+
+  expect(queries.getByRole('link', { name: /name ?\(asc\)/i })).toBeInTheDocument();
+  expect(queries.getByRole('link', { name: /value ?\(unsorted\)/i })).toBeInTheDocument();
+
+  const propsSortedByNameAsc = getProps(queries);
+  expect(propsSortedByNameAsc).toEqual(expectedProps.sortedByNameAsc);
+
+  userEvent.click(queries.getByRole('link', { name: /name ?\(asc\)/i }));
+  expect(queries.getByRole('link', { name: /name ?\(desc\)/i })).toBeInTheDocument();
+  expect(queries.getByRole('link', { name: /value ?\(unsorted\)/i })).toBeInTheDocument();
+  const propsSortedByNameDesc = getProps(queries);
+  expect(propsSortedByNameDesc).toEqual(expectedProps.sortedByNameDesc);
+
+  userEvent.click(queries.getByRole('link', { name: /value ?\(unsorted\)/i }));
+  expect(queries.getByRole('link', { name: /name ?\(unsorted\)/i })).toBeInTheDocument();
+  expect(queries.getByRole('link', { name: /value ?\(asc\)/i })).toBeInTheDocument();
+  const propsSortedByValueAsc = getProps(queries);
+  expect(propsSortedByValueAsc).toEqual(expectedProps.sortedByValueAsc);
+
+  userEvent.click(queries.getByRole('link', { name: /value ?\(asc\)/i }));
+  expect(queries.getByRole('link', { name: /name ?\(unsorted\)/i })).toBeInTheDocument();
+  expect(queries.getByRole('link', { name: /value ?\(desc\)/i })).toBeInTheDocument();
+  const propsSortedByValueDesc = getProps(queries);
+  expect(propsSortedByValueDesc).toEqual(expectedProps.sortedByValueDesc);
+
+  userEvent.click(queries.getByRole('link', { name: /name ?\(unsorted\)/i }));
+  expect(queries.getByRole('link', { name: /name ?\(asc\)/i })).toBeInTheDocument();
+  expect(queries.getByRole('link', { name: /value ?\(unsorted\)/i })).toBeInTheDocument();
+  const propsSortedByNameAsc2 = getProps(queries);
+  expect(propsSortedByNameAsc2).toEqual(expectedProps.sortedByNameAsc);
+});
---
+++ 19 Frontend Architecture/07-organization-of-interface-texts-grid/src/application.js
@@ -0,0 +1,11 @@
+/* eslint no-restricted-syntax: ["off", "ForInStatement"] */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+/* eslint-disable guard-for-in */
+
+import i18next from 'i18next';
+import onChange from 'on-change';
+import resources from './locales';
+
+// BEGIN (write your solution here)
+
+// END
