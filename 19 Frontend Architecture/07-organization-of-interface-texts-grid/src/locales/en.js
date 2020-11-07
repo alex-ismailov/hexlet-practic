7043e347622078ec1ed3a2792bbef1d9a9e6d4e0
smlnx <alexander.is.dev@gmail.com>
2020-11-08 02:16:57 +0300

    add(19 Frontend Architecture/07-organization-of-interface-texts-grid/src/locales/) en.js

--- 19 Frontend Architecture/07-organization-of-interface-texts-grid/src/locales/en.js
+++ 19 Frontend Architecture/07-organization-of-interface-texts-grid/src/locales/en.js
@@ -1,3 +1,18 @@
 // BEGIN (write your solution here)
-
+export default {
+  translation: {
+    asc: 'Asc',
+    desc: 'Desc',
+    unsorted: 'Unsorted',
+    name: 'Name',
+    value: 'Value',
+    host: "host",
+    hostname: "hostname",
+    href: "href",
+    origin: "origin",
+    pathname: "pathname",
+    port: "port",
+    protocol: "protocol",
+  }
+};
 // END
