d23f138776d9894e844f37aafba26cfa20adebc5
smlnx <alexander.is.dev@gmail.com>
2020-11-08 19:01:58 +0300

    refactor(19 Frontend Architecture/07-organization-of-interface-texts-grid/src/) application.js

--- 19 Frontend Architecture/07-organization-of-interface-texts-grid/src/application.js
+++ 19 Frontend Architecture/07-organization-of-interface-texts-grid/src/application.js
@@ -50,20 +50,72 @@ const getLocationProps = () => {
   /* ******************** */
 };
 
+// const handleTabHeadClick = (e) => {
+//   e.preventDefault();
+// };
+
+const handleTabHeadClick = (watchedState) => (e) => {
+  e.preventDefault();
+  const { target } = e;
+  const { activeColumnName, order } = watchedState.grid;
+
+  // определить тип кликнутого name || value
+  const targetColumnName = target.textContent.toLowerCase().includes('name')
+    ? 'name'
+    : 'value';
+
+  if (activeColumnName !== targetColumnName) {
+    watchedState.grid.activeColumnName = targetColumnName;
+  }
+
+  const oppositeOrder = order === 'asc' ? 'desc' : 'asc';
+
+  watchedState.grid.order = oppositeOrder;
+};
+
+
 const render = (watchedState) => {
+  const { activeColumnName, order } = watchedState.grid;
+
   const container = document.querySelector('.container');
+  container.innerHTML = '';
+
   const table = document.createElement('table');
   table.classList.add('table');
   const tbody = document.createElement('tbody');
 
-  const trFirst = `<th><a href="">${i18next.t('name')} (${i18next.t(watchedState.grid.nameOrder)})</a></th>
-                   <th><a href="">${i18next.t('value')} (${i18next.t(watchedState.grid.valueOrder)})</a></th>`;
+  const nameOrderKey = activeColumnName === 'name' ? order : 'unsorted';
+  const valueOrderKey = nameOrderKey === 'unsorted' ? order : 'unsorted';
+
+  // console.log(nameOrderKey);
+  // console.log(valueOrderKey);
+
+  const trFirst = `<th><a href="">${i18next.t('name')} (${i18next.t(nameOrderKey)})</a></th>
+                   <th><a href="">${i18next.t('value')} (${i18next.t(valueOrderKey)})</a></th>`;  
   tbody.innerHTML = trFirst;
 
-  const locationProps = getLocationProps();
-  console.log(locationProps);
+  const thLinks = tbody.querySelectorAll('a');
+  thLinks.forEach((thLink) => thLink.addEventListener('click', handleTabHeadClick(watchedState)));
 
-  const trRows = Object.entries(locationProps)
+  
+  const locationProps = getLocationProps();
+  const locationPropsEntries = Object.entries(locationProps);
+
+  if (activeColumnName === 'name') {
+    if (order === 'asc') {
+      locationPropsEntries.sort(([aKey], [bKey]) => aKey.localeCompare(bKey), 'en-US', { numeric: true });
+    } else {
+      locationPropsEntries.sort(([aKey], [bKey]) => bKey.localeCompare(aKey), 'en-US', { numeric: true });
+    }
+  } else {
+    if (order === 'asc') {
+      locationPropsEntries.sort(([, aValue], [, bValue]) => aValue.localeCompare(bValue), 'en-US', { numeric: true });
+    } else {
+      locationPropsEntries.sort(([, aValue], [, bValue]) => bValue.localeCompare(aValue), 'en-US', { numeric: true });
+    }
+  }
+
+  const trRows = locationPropsEntries
     .map(([key, value]) => {
       const tr = document.createElement('tr');
       const tdName = document.createElement('td');
@@ -76,8 +128,8 @@ const render = (watchedState) => {
 
       return tr;
     });
-
-  trRows.forEach((tr) => tbody.append(tr));
+  
+  tbody.append(...trRows);
   table.append(tbody);
   container.append(table);
 };
@@ -86,14 +138,32 @@ const render = (watchedState) => {
 export default () => {
   const state = {
     grid: {
-      nameOrder: 'asc',
-      valueOrder: 'unsorted',
+      activeColumnName: 'name', // || 'value'
+      order: 'asc', // by default || desc
     },
   };
 
   const watchedState = onChange(state, (path, value, prevValue) => {
-    // ...
+    switch (path) {
+      case 'grid.activeColumnName':
+        break;
+      
+      case 'grid.order':
+        render(watchedState);
+        break;
+      
+      default:
+        break; // throw new Exception
+    }
   });
+
+  // const oppositeOrder = watchedState.grid.order === 'asc' ? 'desc' : 'asc';
+  // render(watchedState.grid.order, oppositeOrder);
+  // render(
+  //   watchedState.grid.activeColumnName,
+  //   watchedState.grid.order,
+  //   oppositeOrder
+  // );
 
   render(watchedState);
 };
