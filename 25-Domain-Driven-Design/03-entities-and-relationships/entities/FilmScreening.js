3730ef052544ad86696adc1b9b226d77e088b99b
smlnx <alexander.is.dev@gmail.com>
2020-11-09 10:49:58 +0300

    add(25-Domain-Driven-Design/03-entities-and-relationships/entities/) FilmScreening.js

--- 25-Domain-Driven-Design/03-entities-and-relationships/entities/FilmScreening.js
+++ 25-Domain-Driven-Design/03-entities-and-relationships/entities/FilmScreening.js
@@ -1,5 +1,14 @@
+import { fi } from 'date-fns/locale';
 import { v4 as uuid } from 'uuid';
 
 // BEGIN (write your solution here)
-
+export default class FilmScreening {
+  constructor(film, cinemaHall, time) {
+    this.id = uuid();
+    this.film = film;
+    this.cinemaHall = cinemaHall;
+    this.time = time;
+    this.createdAt = new Date();
+  }
+}
 // END
