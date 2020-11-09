91c0a102623a8c78915a9fc1c1051152099bf92a
smlnx <alexander.is.dev@gmail.com>
2020-11-09 10:49:05 +0300

    add(25-Domain-Driven-Design/03-entities-and-relationships/entities/) CinemaHall.js

--- 25-Domain-Driven-Design/03-entities-and-relationships/entities/CinemaHall.js
+++ 25-Domain-Driven-Design/03-entities-and-relationships/entities/CinemaHall.js
@@ -1,5 +1,18 @@
 import { v4 as uuid } from 'uuid';
 
 // BEGIN (write your solution here)
+export default class CinemaHall {
+  constructor(name, rows, cols, filmScreenings = []) {
+    this.id = uuid();
+    this.name = name;
+    this.rows = rows;
+    this.cols = cols;
+    this.filmScreenings = filmScreenings; // ???
+    this.createdAt = new Date();
+  }
 
+  addFilmScreening(filmScreening) {
+    this.filmScreenings.push(filmScreening);
+  }
+}
 // END
