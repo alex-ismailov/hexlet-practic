a06355905eaf32511d1c6d4d96959d2c404d8ba0
smlnx <alexander.is.dev@gmail.com>
2020-11-09 10:49:33 +0300

    add(25-Domain-Driven-Design/03-entities-and-relationships/entities/) Film.js

--- 25-Domain-Driven-Design/03-entities-and-relationships/entities/Film.js
+++ 25-Domain-Driven-Design/03-entities-and-relationships/entities/Film.js
@@ -1,5 +1,12 @@
 import { v4 as uuid } from 'uuid';
 
 // BEGIN (write your solution here)
-
+export default class Film {
+  constructor(name, duration) {
+    this.id = uuid();
+    this.name = name;
+    this.duration = duration;
+    this.createdAt = new Date();
+  }
+}
 // END
