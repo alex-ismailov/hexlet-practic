1eca6f76369f4ba91cf862e250b48b7bf7879031
smlnx <alexander.is.dev@gmail.com>
2020-11-16 08:28:04 +0300

    Refactor(25-Domain-Driven-Design/04 architecture/lib/) BaseRepository.js

--- 25-Domain-Driven-Design/04 architecture/lib/BaseRepository.js
+++ 25-Domain-Driven-Design/04 architecture/lib/BaseRepository.js
@@ -1,15 +1,19 @@
+import { exceptions } from "winston";
+
 // BEGIN (write your solution here)
 export default class {
-  constructor() {
-    this.repository = [];
-  }
+  data = []
 
   save(entity) {
-    this.repository = [...this.repository, entity];
+    this.data = [...this.data, entity];
   }
 
   find(id) {
-    return this.repository.find((item) => item.id === id);
+    const result = this.data.find((entity) => entity.id === id);
+    if (!result) {
+      throw new Error('Entity not found');
+    }
+    return result;
   }
 }
 // END
