6633d44bfda84587eb3dfb7a4386c88e1577768c
smlnx <alexander.is.dev@gmail.com>
2020-11-16 08:22:43 +0300

    Add(25-Domain-Driven-Design/04 architecture/lib/) BaseRepository.js implementation

--- 25-Domain-Driven-Design/04 architecture/lib/BaseRepository.js
+++ 25-Domain-Driven-Design/04 architecture/lib/BaseRepository.js
@@ -1,3 +1,15 @@
 // BEGIN (write your solution here)
+export default class {
+  constructor() {
+    this.repository = [];
+  }
 
+  save(entity) {
+    this.repository = [...this.repository, entity];
+  }
+
+  find(id) {
+    return this.repository.find((item) => item.id === id);
+  }
+}
 // END
