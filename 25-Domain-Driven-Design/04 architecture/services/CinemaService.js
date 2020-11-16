ca57f4451cce3713f9f811b22800f73d531f8b8e
smlnx <alexander.is.dev@gmail.com>
2020-11-16 08:23:28 +0300

    Add(25-Domain-Driven-Design/04 architecture/services/) CinemaService.js implementation

--- 25-Domain-Driven-Design/04 architecture/services/CinemaService.js
+++ 25-Domain-Driven-Design/04 architecture/services/CinemaService.js
@@ -3,7 +3,20 @@ import { Film, CinemaHall, FilmScreening } from '../entities';
 
 export default class extends ApplicationService {
   // BEGIN (write your solution here)
-  
+  createCinemaHall(name, rows, cols) {
+    const cinemaHall = new CinemaHall(name, rows, cols);
+    this.CinemaHallRepository.save(cinemaHall);
+    return cinemaHall;
+  }
+
+  createFilmScreening(filmId, cinemaHallId, time) {
+    const film = this.FilmRepository.find(filmId);
+    const cinemaHall = this.CinemaHallRepository.find(cinemaHallId);
+
+    const filmScreening = new FilmScreening(film, cinemaHall, time);
+    this.FilmScreeningRepository.save(filmScreening);
+    return filmScreening;
+  }
   // END
 
   createFilm(name, duration) {
