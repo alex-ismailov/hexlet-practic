eb6c8e67d21bed82632d252c84dd35b5492b91c3
smlnx <alexander.is.dev@gmail.com>
2020-11-18 23:25:08 +0300

    Implement(24-new-Diving-into-classes/02-inheritance/) HTMLHrElement.js

--- 24-new-Diving-into-classes/02-inheritance/HTMLHrElement.js
+++ 24-new-Diving-into-classes/02-inheritance/HTMLHrElement.js
@@ -1,5 +1,10 @@
 import HTMLElement from './HTMLElement.js';
 
 // BEGIN (write your solution here)
-
+export default class HTMLHrElement extends HTMLElement {
+  toString() {
+    const attrLine = this.stringifyAttributes();
+    return `<hr${attrLine}>`;
+  }
+}
 // END
