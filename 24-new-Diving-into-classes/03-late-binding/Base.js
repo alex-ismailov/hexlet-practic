64cbf9607247e7788e61893f9c75ea04dcb471e1
smlnx <alexander.is.dev@gmail.com>
2020-11-19 23:56:56 +0300

    Implement(24-new-Diving-into-classes/03-late-binding/) Base.js - tests pass

--- 24-new-Diving-into-classes/03-late-binding/Base.js
+++ 24-new-Diving-into-classes/03-late-binding/Base.js
@@ -2,6 +2,19 @@
 
 export default class Base {
   // BEGIN (write your solution here)
-  
+  isInstanceOf(className) {
+
+    if (className === this.constructor.name) {
+      return true;
+    }
+
+    const protoOfThis = Object.getPrototypeOf(this);
+
+    if (protoOfThis === Object.prototype) {
+      return false;
+    }
+
+    return protoOfThis.isInstanceOf(className);
+  }
   // END
 }
