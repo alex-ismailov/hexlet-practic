13298dd734d57cde7b9b5d0a8a1447610f69c327
smlnx <alexander.is.dev@gmail.com>
2020-11-20 11:18:28 +0300

    Implement(24-new-Diving-into-classes/04-template-method/) HTMLPairElement.js - tests pass

--- 24-new-Diving-into-classes/04-template-method/HTMLPairElement.js
+++ 24-new-Diving-into-classes/04-template-method/HTMLPairElement.js
@@ -1,6 +1,22 @@
 import HTMLElement from './HTMLElement.js';
 
 // BEGIN (write your solution here)
+class HTMLPairElement extends HTMLElement {
+  toString() {
+    const attrLine = this.stringifyAttributes();
+    const tagName = this.getTagName();
+    const body = this.getTextContent();
 
+    return `<${tagName}${attrLine}>${body}</${tagName}>`;
+  }
+
+  getTextContent() {
+    return this.body;
+  }
+
+  setTextContent(body) {
+    this.body = body;
+  }
+}
 // END
 export default HTMLPairElement;
