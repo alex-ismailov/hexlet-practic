ad7c73786b8ed8fdd41a207d0c6d7956fd3ae84e
smlnx <alexander.is.dev@gmail.com>
2020-11-20 11:18:06 +0300

    Implement(24-new-Diving-into-classes/04-template-method/) HTMLDivElement.js - tests pass

--- 24-new-Diving-into-classes/04-template-method/HTMLDivElement.js
+++ 24-new-Diving-into-classes/04-template-method/HTMLDivElement.js
@@ -3,6 +3,16 @@
 import HTMLPairElement from './HTMLPairElement.js';
 
 // BEGIN (write your solution here)
+class HTMLDivElement extends HTMLPairElement {
+  constructor(attributes) {
+    super(attributes);
+    this.tagName = 'div';
+    this.body = '';
+  }
 
+  getTagName() {
+    return this.tagName;
+  }
+}
 // END
 export default HTMLDivElement;
