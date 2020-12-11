32391a76f03a4f13b086f02607ee6d636708e921
smlnx <alexander.is.dev@gmail.com>
2020-12-11 22:36:39 +0300

    Remove(16.1 prototypes/04 Subtype Polymorphism/buildNode.js) comments

--- 16.1 prototypes/04 Subtype Polymorphism/buildNode.js
+++ 16.1 prototypes/04 Subtype Polymorphism/buildNode.js
@@ -1,19 +1,3 @@
-/* buildNode.js
-Реализуйте и экспортируйте функцию по умолчанию, задача которой, создавать объект подходящего типа.
-Типы: SingleTag и PairedTag. Список имен тегов, которые относятся к SingleTag: hr, br, img.
-
-PairedTag.js
-Реализуйте тип PairedTag со следующим интерфейсом:
-- Конструктор, который принимает на вход: name, attributes, body, children.
-- Метод toString, который возвращает текстовое представление узла (html) на всю глубину.
-
-SingleTag.js
-Реализуйте тип SingleTag со следующим интерфейсом:
-- Конструктор, который принимает на вход: name, attributes
-- Метод toString, который возвращает текстовое представление узла (html) на всю глубину.
-
-Обратите внимание на то что у SingleTag нет body и children */
-
 import PairedTag from './PairedTag';
 import SingleTag from './SingleTag';
 
